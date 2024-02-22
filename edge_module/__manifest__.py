87b00de5725db2b51d8d7505471d61f504c11e77
Richard Trice <rtrice@edgeautonomy.io>
2024-02-22 09:38:39 -0800

    First Push

---
+++ edge_module/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Edge Autonomy Huntsville Odoo Modifications',
+    'version': '1.0',
+    'summary': 'A custom module for specific needs',
+    'sequence': 10,
+    'description': 'Currently just some CSS modifications to the frontend. More to come!',
+    'category': 'Custom',
+    'website': 'https://www.edgeautonomy.io',
+    'depends': ['base','web'],
+    'assets': {
+        'web.assets_frontend': [
+            'edge_module/static/src/css/custom_styles.css',
+        ],
+    },
+    'data': [
+        # XML/YAML files
+    ],
+    'demo': [
+        # Demo data files
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
