ca323eb582524fe8e0909e6be4bfc99939b7c80d
Richard Trice <rtrice@edgeautonomy.io>
2024-02-22 22:35:41 -0800

    test2

--- edge_module/__manifest__.py
+++ edge_module/__manifest__.py
@@ -9,7 +9,7 @@
     'website': 'https://www.edgeautonomy.io',
     'depends': ['web_enterprise'],
     'assets': {
-        'qweb': [
+        'web.assets_backend': [
             'edge_module/static/src/css/custom_styles.css',
         ],
     },
