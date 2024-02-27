abb961df0a466d1c4c1d2a6680b9c7eec2f6db2f
Jesse Larson <jlarson@edgeautonomy.io>
2024-02-27 14:45:22 -0800

    test

--- edge_module/static/src/css/models/models.py
+++ edge_module/static/src/css/models/models.py
@@ -5,4 +5,4 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    anewfield = fields.Boolean('A New Field', required=True)
+    anewfieldy = fields.Boolean('A New Field', required=True)
