ccd9899ee9d564b4315ebb9516bfb89c1ceb35ef
Richard Trice <rtrice@edgeautonomy.io>
2024-02-27 14:53:06 -0800

    Including a couple of dirs

---
+++ edge_module/controllers/controllers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class C:\users\rtrice\source\repos\odooHsv\odoodev(http.Controller):
+#     @http.route('/c:\users\rtrice\source\repos\odoo_hsv\odoodev/c:\users\rtrice\source\repos\odoo_hsv\odoodev', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/c:\users\rtrice\source\repos\odoo_hsv\odoodev/c:\users\rtrice\source\repos\odoo_hsv\odoodev/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('c:\users\rtrice\source\repos\odoo_hsv\odoodev.listing', {
+#             'root': '/c:\users\rtrice\source\repos\odoo_hsv\odoodev/c:\users\rtrice\source\repos\odoo_hsv\odoodev',
+#             'objects': http.request.env['c:\users\rtrice\source\repos\odoo_hsv\odoodev.c:\users\rtrice\source\repos\odoo_hsv\odoodev'].search([]),
+#         })
+
+#     @http.route('/c:\users\rtrice\source\repos\odoo_hsv\odoodev/c:\users\rtrice\source\repos\odoo_hsv\odoodev/objects/<model("c:\users\rtrice\source\repos\odoo_hsv\odoodev.c:\users\rtrice\source\repos\odoo_hsv\odoodev"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('c:\users\rtrice\source\repos\odoo_hsv\odoodev.object', {
+#             'object': obj
+#         })
+
