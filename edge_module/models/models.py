779b54b22023d544eea9e3334d14533ca249d5a3
Jesse Larson <jlarson@edgeautonomy.io>
2024-02-28 10:47:38 -0800

    fix for the error, removed reuired

--- edge_module/models/models.py
+++ edge_module/models/models.py
@@ -5,4 +5,4 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    procurementcategory = fields.Selection([('Office Supplies'),('Meals')],string='Procurement Category', required=True)
+    procurementcategory = fields.Selection([('officeSupplies','Office Supplies'),('meals', 'Meals')],string='Procurement Category')
