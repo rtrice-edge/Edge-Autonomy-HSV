b09ca0c05eb5b6ac1be99c5cc1e6040a5479061a
Jesse Larson <jlarson@edgeautonomy.io>
2024-02-28 14:09:02 -0800

    dynamic dropdown?

--- edge_module/models/models.py
+++ edge_module/models/models.py
@@ -2,7 +2,26 @@
 
 from odoo import models, fields
 
+
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-    
-    expensetype = fields.Selection([('officeSupplies','Office Supplies'),('meals', 'Meals')],string='Expense Type')
+    _name = 'purchase.order'
+
+    expensetype = fields.Selection([
+        ('direct', 'Direct'),
+        ('g&a', 'G & A'),
+        ('ir&d', 'IR&D')
+    ], string='Expense Type', required=True)
+
+    expensetype2 = fields.Selection(
+        selection='_get_selection_2', string='Expense Type')
+
+    def _get_selection_2(self):
+        # Define SELECTION_LIST_2 and SELECTION_LIST_3 somewhere in your code
+        SELECTION_LIST_2 = [('officesupplies', 'Office Supplies'), ('meal', 'Meals')]
+        SELECTION_LIST_3 = [('officesupplies', 'Office Supplies'), ('meal', 'Meals')]
+        
+        selected = self.expensetype
+        if selected == 'direct':
+            return SELECTION_LIST_2
+        else:
+            return SELECTION_LIST_3
