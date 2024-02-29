9ca093b2eea8afa4bf9508bfaec9d15459b98e55
Jesse Larson <jlarson@edgeautonomy.io>
2024-02-29 06:31:04 -0800

    cascasing dropdowm

--- edge_module/models/models.py
+++ edge_module/models/models.py
@@ -5,5 +5,19 @@ from odoo import models, fields
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
-    
-    expensetype = fields.Selection([('officeSupplies','Office Supplies'),('meals', 'Meals')],string='Expense Type')
+
+    expensetype = fields.Selection([
+        ('direct', 'Direct'),
+        ('g&a', 'G & A'),
+        ('ir&d', 'IR&D')
+    ], string='Expense Type', required=True)
+
+    expensetype2 = fields.Selection(
+        selection='_get_selection_2', string='Expense Type')
+
+    @staticmethod
+    def _get_selection_2():
+        SELECTION_LIST_2 = [('officesupplies', 'Office Supplies'), ('meal', 'Meals')]
+        SELECTION_LIST_3 = [('expense1', 'Expense 1'), ('expense2', 'Expense 2')]
+
+        return SELECTION_LIST_2 + SELECTION_LIST_3
