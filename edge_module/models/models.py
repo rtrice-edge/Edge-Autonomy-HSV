49a8a41988d9b1f184032529e3659347501acf2e
Jesse Larson <jlarson@edgeautonomy.io>
2024-02-29 06:53:08 -0800

    syntax error

--- edge_module/models/models.py
+++ edge_module/models/models.py
@@ -16,19 +16,19 @@ class PurchaseOrder(models.Model):
         selection='_get_selection_2', string='Expense Type')
 
     @staticmethod
-        def _get_selection_2():
-            SELECTION_LIST_2 = [
-                ('direct', [
-                    ('officesupplies', 'Office Supplies'),
-                    ('meal', 'Meals')
-                ]),
-                ('g&a', [
-                    ('officesupplies', 'Office Supplies'),
-                    ('meal', 'Meals')
-                ]),
-                ('ir&d', [
-                    ('officesupplies', 'Office Supplies'),
-                    ('meal', 'Meals')
-                ])
-            ]
-            return SELECTION_LIST_2
+    def _get_selection_2():
+        SELECTION_LIST_2 = [
+            ('direct', [
+                ('officesupplies', 'Office Supplies'),
+                ('meal', 'Meals')
+            ]),
+            ('g&a', [
+                ('officesupplies', 'Office Supplies'),
+                ('meal', 'Meals')
+            ]),
+            ('ir&d', [
+                ('officesupplies', 'Office Supplies'),
+                ('meal', 'Meals')
+            ])
+        ]
+        return SELECTION_LIST_2
