e5b6bc795c2423b87070465d980e4e129a54d95d
Jesse Larson <jlarson@edgeautonomy.io>
2024-02-29 07:12:59 -0800

    change the cascade to two

--- edge_module/models/models.py
+++ edge_module/models/models.py
@@ -6,29 +6,35 @@ from odoo import models, fields
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    expensetype = fields.Selection([
+    costobjective = fields.Selection([
         ('direct', 'Direct'),
         ('g&a', 'G & A'),
-        ('ir&d', 'IR&D')
+        ('engovh', 'Eng OVH'),
+        ('manovh', 'Man OVH'),
+        ('ir&d', 'IR&D'),
+        ('b&p', 'B&P')
+    ], string='Cost Objective', required=True)
+
+    expensetype = fields.Selection([
+        ('engineeringmaterials ', 'Engineering Materials '),
+        ('electronicsmaterials ', 'Electronics Materials '),
+        ('inventory/procurementmaterials ', 'Inventory/Procurement Materials '),
+        ('composites/assemblymaterials ', 'Composites/Assembly Materials '),
+        ('groundsupportmaterials ', 'Ground Support Materials '),
+        ('subklabor-engineering ', 'Subk Labor-Engineering '),
+        ('subklabor-composites/assembly ', 'Subk Labor-Composites/Assembly '),
+        ('packing,postage,andfreight ', 'Packing, Postage, and Freight '),
+        ('smalltooling ', 'Small Tooling '),
+        ('itservices ', 'IT Services '),
+        ('ithardware/peripherals ', 'IT Hardware/Peripherals '),
+        ('itsoftware ', 'IT Software '),
+        ('professionalservices/consultants ', 'Professional Services/Consultants '),
+        ('supplies ', 'Supplies '),
+        ('janitorial ', 'Janitorial '),
+        ('repairs&maintenance ', 'Repairs & Maintenance '),
+        ('equipmentrental ', 'Equipment Rental '),
+        ('flighttesting ', 'Flight Testing '),
+        ('smalltestequipment ', 'Small Test Equipment '),
+        ('wastedisposal ', 'Waste Disposal '),
+        ('safety ', 'Safety '),
     ], string='Expense Type', required=True)
-
-    expensetype2 = fields.Selection(
-        selection='_get_selection_2', string='Expense Type')
-
-    @staticmethod
-    def _get_selection_2():
-        SELECTION_LIST_2 = [
-            ('direct', [
-                ('officesupplies', 'Office Supplies'),
-                ('meal', 'Meals')
-            ]),
-            ('g&a', [
-                ('officesupplies', 'Office Supplies'),
-                ('meal', 'Meals')
-            ]),
-            ('ir&d', [
-                ('officesupplies', 'Office Supplies'),
-                ('meal', 'Meals')
-            ])
-        ]
-        return SELECTION_LIST_2
