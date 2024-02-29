aa90c51cc48bca72d0750d6ef03568695d6175b0
Jesse Larson <jlarson@edgeautonomy.io>
2024-02-29 13:58:10 -0800

    revert changes

--- edge_module/models/models1.py
+++
@@ -1,9 +0,0 @@
-#odoo procurement category
-
-from odoo import models, fields
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    vendornumber = fields.Character('Vendor Number', required=True)
