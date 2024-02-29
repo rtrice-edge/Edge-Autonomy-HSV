84ca744773e0b5ff269ee3858e2c806fbb06c411
Jesse Larson <jlarson@edgeautonomy.io>
2024-02-29 08:02:17 -0800

    change model to line

--- edge_module/models/models.py
+++ edge_module/models/models.py
@@ -3,8 +3,8 @@
 from odoo import models, fields
 
 
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
 
     costobjective = fields.Selection([
         ('direct', 'Direct'),
