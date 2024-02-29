933be8c418262245e723e92261c82b2a8e015d00
Jesse Larson <jlarson@edgeautonomy.io>
2024-02-29 06:22:57 -0800

    revert to what worked

--- edge_module/models/models.py
+++ edge_module/models/models.py
@@ -5,19 +5,5 @@ from odoo import models, fields
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
-
-    expensetype = fields.Selection([
-        ('direct', 'Direct'),
-        ('g&a', 'G & A'),
-        ('ir&d', 'IR & D')
-    ], string='Expense Type')
-
-    expensetype = fields.Selection(
-        selection='_get_selection_2', string='Expense Type')
-
-    @staticmethod
-    def _get_selection_2():
-        SELECTION_LIST_2 = [('officesupplies', 'Office Supplies'), ('meal', 'Meals')]
-        SELECTION_LIST_3 = [('expense1', 'Expense 1'), ('expense2', 'Expense 2')]
-
-        return SELECTION_LIST_2 + SELECTION_LIST_3
+    
+    expensetype = fields.Selection([('officeSupplies','Office Supplies'),('meals', 'Meals')],string='Expense Type',required=True)
