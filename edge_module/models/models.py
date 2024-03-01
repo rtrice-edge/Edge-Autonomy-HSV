819f654dc74298a89470a1363ef93de8f9627485
Jesse Larson <jlarson@edgeautonomy.io>
2024-03-01 13:29:21 -0800

    change to boolean

--- edge_module/models/models.py
+++ edge_module/models/models.py
@@ -69,5 +69,5 @@ class StockMoveLine(models.Model):
 
     noninventorymanufacturernumber = fields.Char(string='Non-Inventory Manufacturer Number')
 
-    receiptsfai = fields.Char(string='First Article Inspection (FAI)', readonly=True)
+    receiptsfai = fields.Boolean(string='First Article Inspection (FAI)', readonly=True)
 
