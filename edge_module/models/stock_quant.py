a78ae740ab2a2dc061309a79f6d1e64b0ae2777c
Richard Trice <rtrice@edgeautonomy.io>
2024-03-13 06:34:44 -0700

    glub glub glub

--- edge_module/models/stock_quant.py
+++ edge_module/models/stock_quant.py
@@ -3,19 +3,14 @@ from odoo import models, api
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    def print_lots(self):
-
+    def print_lots_action(self):
         lot_data = []
         for quant in self:
-            # lot_data.append({
-            #     'product': quant.product_id.name,
-            #     'lot': quant.lot_id.name
-            # })
             doc = type('', (), {})()  # Create an empty object
             doc.product_id = type('', (), {})()  # Create an empty object for product_id
             doc.product_id.display_name = quant.product_id.display_name
             doc.product_id.default_code = quant.product_id.name
             doc.product_id.name = quant.lot_id.name
             lot_data.append(doc)
-        return self.env.ref('stock.report_lot_label').report_action(self, data={'docs': lot_data})
+        return self.env.ref('stock.action_report_lot_label').report_action(lot_data)
     
