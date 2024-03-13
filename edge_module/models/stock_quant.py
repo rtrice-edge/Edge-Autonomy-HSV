fef63fc45eee3ccefe2156a0f71346fbd9ceb424
Richard Trice <rtrice@edgeautonomy.io>
2024-03-13 07:13:23 -0700

    another round

--- edge_module/models/stock_quant.py
+++ edge_module/models/stock_quant.py
@@ -1,27 +1,38 @@
 from odoo import models, api
 
 import logging
-
+import json
 _logger = logging.getLogger(__name__)
 
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    def print_lots_action(self):
-        lot_data = []
-        
-        print("Self:", self)  # Add this line to inspect the self object
-        for quant in self:
-            print(self)
-            doc = type('', (), {})()  # Create an empty object
-            doc.product_id = type('', (), {})()  # Create an empty object for product_id
-            doc.product_id.display_name = quant.product_id.display_name
-            doc.product_id.default_code = quant.product_id.name
-            doc.product_id.name = quant.lot_id.name
-            lot_data.append(doc)
-        print("Lot Data:", lot_data)  # Add this line to inspect the lot_data list
-        _logger.info("Lot Data: %s", lot_data)
-        return self.env.ref('stock.action_report_lot_label').report_action(self, data=lot_data)
+def print_lots_action(self):
+    lot_data = []
+    for quant in self:
+        lot_data.append({
+            'product_id': {
+                'display_name': quant.product_id.display_name,
+                'default_code': quant.product_id.name,
+                'name': quant.lot_id.name,
+            },
+        })
     
+    report_action = self.env.ref('stock.action_report_lot_label')
+    report_options = {
+        'report_name': report_action.report_name,
+        'report_file': report_action.report_file,
+        'report_type': report_action.report_type,
+    }
+    
+    return {
+        'type': 'ir.actions.report',
+        'report_name': report_action.report_name,
+        'report_type': report_action.report_type,
+        'data': {
+            'options': json.dumps(report_options),
+            'docs': lot_data,
+        },
+    }
     
