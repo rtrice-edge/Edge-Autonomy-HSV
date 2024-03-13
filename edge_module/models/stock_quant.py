679576e16560093ee5941cc56610b2be2b3d1725
Richard Trice <rtrice@edgeautonomy.io>
2024-03-13 07:15:18 -0700

    ug.  indenting is hard

--- edge_module/models/stock_quant.py
+++ edge_module/models/stock_quant.py
@@ -8,31 +8,31 @@ _logger = logging.getLogger(__name__)
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-def print_lots_action(self):
-    lot_data = []
-    for quant in self:
-        lot_data.append({
-            'product_id': {
-                'display_name': quant.product_id.display_name,
-                'default_code': quant.product_id.name,
-                'name': quant.lot_id.name,
+    def print_lots_action(self):
+        lot_data = []
+        for quant in self:
+            lot_data.append({
+                'product_id': {
+                    'display_name': quant.product_id.display_name,
+                    'default_code': quant.product_id.name,
+                    'name': quant.lot_id.name,
+                },
+            })
+        
+        report_action = self.env.ref('stock.action_report_lot_label')
+        report_options = {
+            'report_name': report_action.report_name,
+            'report_file': report_action.report_file,
+            'report_type': report_action.report_type,
+        }
+        
+        return {
+            'type': 'ir.actions.report',
+            'report_name': report_action.report_name,
+            'report_type': report_action.report_type,
+            'data': {
+                'options': json.dumps(report_options),
+                'docs': lot_data,
             },
-        })
-    
-    report_action = self.env.ref('stock.action_report_lot_label')
-    report_options = {
-        'report_name': report_action.report_name,
-        'report_file': report_action.report_file,
-        'report_type': report_action.report_type,
-    }
-    
-    return {
-        'type': 'ir.actions.report',
-        'report_name': report_action.report_name,
-        'report_type': report_action.report_type,
-        'data': {
-            'options': json.dumps(report_options),
-            'docs': lot_data,
-        },
-    }
+        }
     
