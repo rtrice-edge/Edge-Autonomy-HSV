45769bce05a53562b9c1386de04958ea48bc7d14
Richard Trice <rtrice@edgeautonomy.io>
2024-03-13 12:38:58 -0700

    asdfasdf

--- edge_module/models/stock_quant.py
+++ edge_module/models/stock_quant.py
@@ -8,10 +8,9 @@ _logger = logging.getLogger(__name__)
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    def print_lots_action(self,record_ids):
+    def print_lots_action(self, record_ids):
         lot_data = []
-        docids = record_ids
-        for quant in self:
+        for quant in self.browse(record_ids):
             lot_data.append({
                 'product_id': {
                     'display_name': quant.product_id.display_name,
@@ -19,21 +18,22 @@ class StockQuant(models.Model):
                     'name': quant.lot_id.name,
                 },
             })
-            docids.append(quant.id)
-        
+
         report_action = self.env.ref('stock.action_report_lot_label')
         report_options = {
             'report_name': report_action.report_name,
             'report_file': report_action.report_file,
             'report_type': report_action.report_type,
         }
+
         _logger.info('docs: %s', lot_data)
         _logger.info('options: %s', report_options)
-        _logger.info('docids: %s', docids)
-        return report_action.report_action(docids,data={
-                'options': json.dumps(report_options),
-                'docs': lot_data,
-            })
+        _logger.info('record_ids: %s', record_ids)
+        
+        return report_action.report_action(record_ids, data={
+            'options': report_options,
+            'docs': lot_data,
+        })
 
         
     
