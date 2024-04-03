1dc8866e28c9664d03b58cb31c74a4006136b840
Richard Trice <rtrice@edgeautonomy.io>
2024-04-03 11:20:09 -0700

    asdfasdf

--- edge_module/static/src/js/custom_form_view.js
+++ edge_module/static/src/js/custom_form_view.js
@@ -9,9 +9,11 @@ export class CustomFormRenderer extends FormRenderer {
 
     _renderTagSelect(node) {
         var $select = this._super(node);
-
+        console.log("I'm inside the custom form renderer");
         if (node.attrs.name === 'urgency') {
+            console.log("I'm inside the urgency select")
             $select.find('option').each(function () {
+                console.log("I'm inside the urgency select option")
                 var $option = $(this);
                 var value = $option.val();
 
@@ -19,9 +21,11 @@ export class CustomFormRenderer extends FormRenderer {
 
                 switch (value) {
                     case 'low':
+                        
                         $option.addClass('btn-primary');
                         break;
                     case 'medium':
+                        console.log("I'm inside the medium urgency")
                         $option.addClass('btn-warning');
                         break;
                     case 'high':
