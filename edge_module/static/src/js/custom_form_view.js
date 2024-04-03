3e3f934dec4b6475a13cdb4e2b4f92eec03f482b
Richard Trice <rtrice@edgeautonomy.io>
2024-04-03 10:02:02 -0700

    tets

--- edge_module/static/src/js/custom_form_view.js
+++ edge_module/static/src/js/custom_form_view.js
@@ -5,17 +5,17 @@ odoo.define('edge_module.custom_form_view',[], function (require) {
 
     FormRenderer.extend({
         _renderTagSelect: function (node) {
-            console.log("I made it into here, and we can see the node: ", node.attrs.name)
+            // console.log("I made it into here, and we can see the node: ", node.attrs.name)
             var $select = this._super.apply(this, arguments);
 
             if (node.attrs.name === 'urgency') {
-                console.log("I found the urgency and its great!")
+                // console.log("I found the urgency and its great!")
                 $select.find('option').each(function () {
                     var $option = $(this);
                     var value = $option.val();
                     
                     $option.addClass('btn');
-                    console.log("I found the options!")
+                    // console.log("I found the options!")
                     switch (value) {
                         case 'low':
                             $option.addClass('btn-primary');
