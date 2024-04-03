1a6f12c9473e150174e3474e30c345f170b27404
Richard Trice <rtrice@edgeautonomy.io>
2024-04-03 11:52:42 -0700

    asdfasdfasdfasdf

--- edge_module/static/src/js/custom_form_view.js
+++ edge_module/static/src/js/custom_form_view.js
@@ -1,43 +1,35 @@
 /** @odoo-module **/
 
-import { FormRenderer } from "@web/views/form/form_renderer";
+import { FieldSelection } from "@web/views/fields/selection/selection_field";
 
-export class CustomFormRenderer extends FormRenderer {
-    setup() {
-        super.setup();
-    }
+export class CustomUrgencyRenderer extends FieldSelection {
+    async _renderEdit(record, node) {
+        const $select = await super._renderEdit(record, node);
+        
+        $select.find('option').each(function () {
+            var $option = $(this);
+            var value = $option.val();
 
-    _renderTagSelect(node) {
-        var $select = this._super(node);
-        console.log("I'm inside the custom form renderer");
-        if (node.attrs.name === 'urgency') {
-            console.log("I'm inside the urgency select")
-            $select.find('option').each(function () {
-                console.log("I'm inside the urgency select option")
-                var $option = $(this);
-                var value = $option.val();
+            $option.addClass('btn');
 
-                $option.addClass('btn');
-
-                switch (value) {
-                    case 'low':
-                        
-                        $option.addClass('btn-primary');
-                        break;
-                    case 'medium':
-                        console.log("I'm inside the medium urgency")
-                        $option.addClass('btn-warning');
-                        break;
-                    case 'high':
-                        $option.addClass('btn-danger');
-                        break;
-                    case 'stoppage':
-                        $option.addClass('btn-danger blinking');
-                        break;
-                }
-            });
-        }
+            switch (value) {
+                case 'low':
+                    $option.addClass('btn-primary');
+                    break;
+                case 'medium':
+                    $option.addClass('btn-warning');
+                    break;
+                case 'high':
+                    $option.addClass('btn-danger');
+                    break;
+                case 'stoppage':
+                    $option.addClass('btn-danger blinking');
+                    break;
+            }
+        });
 
         return $select;
     }
 }
+
+CustomUrgencyRenderer.template = "web.SelectionField";
