a2aef98a3fd0b4910003f0acc064ca6c0bd82f38
Richard Trice <rtrice@edgeautonomy.io>
2024-04-03 09:42:38 -0700

    testtest

---
+++ edge_module/static/src/js/custom_form_view.js
@@ -0,0 +1,39 @@
+odoo.define('edge_module.custom_form_view',[], function (require) {
+    'use strict';
+
+    var FormRenderer = require('web.FormRenderer');
+
+    FormRenderer.extend({
+        _renderTagSelect: function (node) {
+            var $select = this._super.apply(this, arguments);
+
+            if (node.attrs.name === 'urgency') {
+                $select.find('option').each(function () {
+                    var $option = $(this);
+                    var value = $option.val();
+
+                    $option.addClass('btn');
+
+                    switch (value) {
+                        case 'low':
+                            $option.addClass('btn-primary');
+                            break;
+                        case 'medium':
+                            $option.addClass('btn-warning');
+                            break;
+                        case 'high':
+                            $option.addClass('btn-danger');
+                            break;
+                        case 'stoppage':
+                            $option.addClass('btn-danger blinking');
+                            break;
+                    }
+                });
+            }
+
+            return $select;
+        },
+    });
+    return {Renderer: FormRenderer};
+   
+});
