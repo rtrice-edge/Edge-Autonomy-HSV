e8debad194409f19fdba093f60f1db216c07ae85
Richard Trice <rtrice@edgeautonomy.io>
2024-04-08 08:42:30 -0700

    test

--- edge_module/__manifest__.py
+++ edge_module/__manifest__.py
@@ -16,20 +16,27 @@
     },
     'data': [
         # 'security/ir.model.access.csv,
-        'views/views.xml',
+       'views/mrp/mrp_bom_view.xml',
+       
         'views/product/product_fields.xml',
-        'views/stock/stock_quant_print_lots.xml',
-        'views/StockMoveLine.xml',
-        'views/mrp/mrp_bom_view.xml',
-        'views/stock/stock_picking_form.xml',
-        'views/custom_report_generic_barcode.xml',
-        # 'views/purchase_order/rfq_chatter_view.xml',
-        'data/custom_paper_format.xml',
-        'reports/purchase_order_custom.xml',
-        'reports/request_for_quotation.xml',
-        'reports/delivery_report.xml',
-        'security/ir.model.access.csv',
+        'views/product/product_product_normal_form_view_inherit.xml',
+        'views/product/product_template_form_inherit.xml',
+        
+        'views/project/project_form_inherit.xml',
+        
+        'views/purchase_order/po_report.xml',
+        'views/purchase_order/purchase_order_form_inherit.xml',
+        'views/purchase_order/purchase_order_kpis_tree_inherit_urgency.xml',
+        'views/purchase_order/purchase_order_search_inherit.xml',
+        'views/purchase_order/rfq_chatter_view.xml',
+        
         'views/sale_order/sale_order_form.xml',
+        
+        'views/stock/stock_move_line.xml',
+        'views/stock/stock_picking_form.xml',
+        'views/stock/stock_quant_print_lots.xml',
+        'views/stock/stock_report_generic_barcode.xml',
+        
         'views/theme/theme.xml',
     ],
     'demo': [
