3b37bfb5427c6d315bbd1e8a35eda921097a62c2
Richard Trice <rtrice@edgeautonomy.io>
2024-04-15 05:35:30 -0700

    test

--- edge_module/models/purchase_requisition.py
+++ edge_module/models/purchase_requisition.py
@@ -3,6 +3,20 @@ from odoo import api, models, fields
 import logging
 _logger = logging.getLogger(__name__)
 
+class PurchaseRequisition(models.Model):
+    _inherit = "purchase.requisition"
+
+    def button_confirm(self):
+        _logger.info('Called button_confirm')
+        res = super(PurchaseRequisition, self).button_confirm()
+        for requisition in self:
+            for po in requisition.purchase_ids:
+                for line in po.order_line:
+                    if line.requisition_line_id:
+                        _logger.info('Updating product description from PR line')
+                        line.name = line.requisition_line_id.product_description_variants or line.name
+        return res
+
 class PurchaseRequisitionLine(models.Model):
     _inherit = "purchase.requisition.line"
 
