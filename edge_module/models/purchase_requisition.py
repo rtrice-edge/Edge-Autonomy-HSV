d580886db6e1c22e52bd3766f0ac41554f9b4013
Richard Trice <rtrice@edgeautonomy.io>
2024-04-15 05:19:14 -0700

    test

--- edge_module/models/purchase_requisition.py
+++ edge_module/models/purchase_requisition.py
@@ -9,6 +9,7 @@ class PurchaseRequisitionLine(models.Model):
     move_dest_id = fields.Many2one('stock.move', 'Downstream Move')
 
     def _prepare_purchase_order_line(self, name, product_qty=0.0, price_unit=0.0, taxes_ids=False):
+        _logger.info('Called _prepare_purchase_order_line')
         res = super(PurchaseRequisitionLine, self)._prepare_purchase_order_line(name, product_qty, price_unit, taxes_ids)
         res['name'] = self.product_description_variants or name
         res['move_dest_ids'] = self.move_dest_id and [(4, self.move_dest_id.id)] or []
