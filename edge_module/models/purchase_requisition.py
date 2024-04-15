7c71cc203a043fb286affcbdf7a907d531989939
Richard Trice <rtrice@edgeautonomy.io>
2024-04-15 05:17:33 -0700

    test

--- edge_module/models/purchase_requisition.py
+++ edge_module/models/purchase_requisition.py
@@ -1,22 +1,15 @@
-from odoo import api, models
+from odoo import api, models, fields
 
 import logging
 _logger = logging.getLogger(__name__)
 
-class PurchaseRequisition(models.Model):
-    _inherit = 'purchase.requisition'
+class PurchaseRequisitionLine(models.Model):
+    _inherit = "purchase.requisition.line"
 
-    def action_create_purchase_order(self):
-        _logger.info('Called action_create_purchase_order')
-        res = super(PurchaseRequisition, self).action_create_purchase_order()
-        for requisition in self:
-            _logger.info('Processing requisition: %s', requisition.name)
-            for po in requisition.purchase_ids:
-                _logger.info('Processing purchase order: %s', po.name)
-                for line in requisition.line_ids:
-                    _logger.info('Processing requisition line: %s', line.product_id.name)
-                    po_line = po.order_line.filtered(lambda l: l.product_id == line.product_id)
-                    if po_line:
-                        _logger.info('Updating purchase order line description')
-                        po_line.name = line.product_description_variants
+    move_dest_id = fields.Many2one('stock.move', 'Downstream Move')
+
+    def _prepare_purchase_order_line(self, name, product_qty=0.0, price_unit=0.0, taxes_ids=False):
+        res = super(PurchaseRequisitionLine, self)._prepare_purchase_order_line(name, product_qty, price_unit, taxes_ids)
+        res['name'] = self.product_description_variants or name
+        res['move_dest_ids'] = self.move_dest_id and [(4, self.move_dest_id.id)] or []
         return res
