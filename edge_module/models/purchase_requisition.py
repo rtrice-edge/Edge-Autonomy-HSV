dfe46c57ad2058d7d8d9e1f5fe4ff0c36074c27e
Richard Trice <rtrice@edgeautonomy.io>
2024-04-15 06:03:14 -0700

    testsetsea

--- edge_module/models/purchase_requisition.py
+++ edge_module/models/purchase_requisition.py
@@ -3,27 +3,15 @@ from odoo import api, models, fields
 import logging
 _logger = logging.getLogger(__name__)
 
-class PurchaseRequisition(models.Model):
-    _inherit = "purchase.requisition"
-
-    def button_confirm(self):
-        _logger.info('Called button_confirm')
-        res = super(PurchaseRequisition, self).button_confirm()
-        for requisition in self:
-            for po in requisition.purchase_ids:
-                for line in po.order_line:
-                    if line.requisition_line_id:
-                        _logger.info('Updating product description from PR line')
-                        line.name = line.requisition_line_id.product_description_variants or line.name
-        return res
 
 class PurchaseRequisitionLine(models.Model):
     _inherit = "purchase.requisition.line"
 
 
     def _prepare_purchase_order_line(self, name, product_qty=0.0, price_unit=0.0, taxes_ids=False):
+        _logger.info('Called _prepare_purchase_order_line')
+        name = self.product_description_variants or name
+        _logger.info(name + "name")
         res = super(PurchaseRequisitionLine, self)._prepare_purchase_order_line(name, product_qty, price_unit, taxes_ids)
-        res['name'] = self.product_description_variants or name
-        _logger.info(res['name'] + "res[name]")
         res['move_dest_ids'] = self.move_dest_id and [(4, self.move_dest_id.id)] or []
         return res
