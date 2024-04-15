bfb67c979c0d2e542e0d79ad84b25f78ec84484c
Richard Trice <rtrice@edgeautonomy.io>
2024-04-15 05:29:08 -0700

    etest

--- edge_module/models/purchase_requisition.py
+++ edge_module/models/purchase_requisition.py
@@ -6,7 +6,6 @@ _logger = logging.getLogger(__name__)
 class PurchaseRequisitionLine(models.Model):
     _inherit = "purchase.requisition.line"
 
-    move_dest_id = fields.Many2one('stock.move', 'Downstream Move')
 
     def _prepare_purchase_order_line(self, name, product_qty=0.0, price_unit=0.0, taxes_ids=False):
         _logger.info('Called _prepare_purchase_order_line')
@@ -14,5 +13,6 @@ class PurchaseRequisitionLine(models.Model):
         _logger.info(self.product_description_variants + "product_description_variants" )
         _logger.info(name + "name")
         res['name'] = self.product_description_variants or name
+        _logger.info(res['name'] + "res[name]")
         res['move_dest_ids'] = self.move_dest_id and [(4, self.move_dest_id.id)] or []
         return res
