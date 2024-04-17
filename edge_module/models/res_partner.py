9ddb056f49adb8aec8c4530957d7d03babf32127
jlars0n <[jlarson@edgeautonomy.io]>
2024-04-17 15:42:03 -0700

    add field to vendor

---
+++ edge_module/models/res_partner.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    vendor_terms = fields.Char(string='Vendor Terms')
