ab2832d5d12e8d6fa3f967a1a4078d52fd1a7c76
Jesse Larson <jlarson@edgeautonomy.io>
2024-04-23 09:58:59 -0700

    this is VERY NICE

--- edge_module/models/stock_picking.py
+++ edge_module/models/stock_picking.py
@@ -4,3 +4,5 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     tracking_number = fields.Char(string='Tracking Number')
+    carrier = fields.Char(string='Carrier')
+    shipping_price = fields.Monetary(string='Shipping Price')
