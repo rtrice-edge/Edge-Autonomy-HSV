97504193bbb5c4ab4eb4a7ea4ce32bee143d5df8
Jesse Larson <jlarson@edgeautonomy.io>
2024-04-23 10:40:28 -0700

    mh

--- edge_module/models/stock_picking.py
+++ edge_module/models/stock_picking.py
@@ -5,5 +5,3 @@ class StockPicking(models.Model):
 
     tracking_number = fields.Char(string='Tracking Number')
     carrier = fields.Char(string='Carrier')
-    currency_deliveries = fields.Currency(string="Currency")
-    shipping_price = fields.Monetary(string='Shipping Price', default=0.0)
