30fb9c5fcf5288205b5cc2c86215b85456d529d8
Jesse Larson <jlarson@edgeautonomy.io>
2024-04-23 10:49:35 -0700

    salty

--- edge_module/models/stock_picking.py
+++ edge_module/models/stock_picking.py
@@ -6,3 +6,4 @@ class StockPicking(models.Model):
     tracking_number = fields.Char(string='Tracking Number')
     carrier = fields.Char(string='Carrier')
     currency_id = fields.Many2one(string='Currency', related='company_id.currency_id', readonly=True)
+    delivery_price = fields.Monetary('Delivery Cost', currency_field='currency_id', default=0.0)
