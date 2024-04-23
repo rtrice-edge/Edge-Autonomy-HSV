31bee2eade0abe1d969f658689a1692a9e7e6bf1
Jesse Larson <jlarson@edgeautonomy.io>
2024-04-23 16:02:59 -0700

    mini-wheats

---
+++ edge_module/models/product_packaging.py
@@ -0,0 +1,11 @@
+from odoo import models, fields, api
+
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class PProductPackaging(models.Model):
+    _inherit = 'product.packaging'
+
+    currency_id = fields.Many2one(string='Currency', related='company_id.currency_id', readonly=True)
+    package_price = fields.Monetary('Package Price', currency_field='currency_id', default=0.0)
