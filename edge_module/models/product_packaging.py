9aff8e479498c16b51871059cd036ad9966eb15d
Jesse Larson <jlarson@edgeautonomy.io>
2024-04-23 16:46:03 -0700

    for claude

--- edge_module/models/product_packaging.py
+++ edge_module/models/product_packaging.py
@@ -1,11 +1,10 @@
 from odoo import models, fields, api
-
-
 import logging
 _logger = logging.getLogger(__name__)
 
 class ProductPackaging(models.Model):
     _inherit = 'product.packaging'
 
-    prod_currency_id = fields.Many2one(string='Currency', related='company_id.prod_currency_id', readonly=True)
-    package_price = fields.Monetary('Package Price', currency_field='prod_currency_id', default=0.0)
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
+    currency_id = fields.Many2one('res.currency', string='Currency', related='company_id.currency_id', readonly=True)
+    package_price = fields.Monetary('Package Price', currency_field='currency_id', default=0.0)
