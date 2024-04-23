5393fb2a3dc690504d014d13570a1b4b27739ce8
Jesse Larson <jlarson@edgeautonomy.io>
2024-04-23 16:49:15 -0700

    iuh qwlieuqade

--- edge_module/models/product_packaging.py
+++ edge_module/models/product_packaging.py
@@ -5,6 +5,5 @@ _logger = logging.getLogger(__name__)
 class ProductPackaging(models.Model):
     _inherit = 'product.packaging'
 
-    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
-    currency_id = fields.Many2one('res.currency', string='Currency', related='company_id.currency_id', readonly=True)
-    package_price = fields.Monetary('Package Price', currency_field='currency_id', default=0.0)
+    packaging_currency_id = fields.Many2one('res.currency', string='Packaging Currency', related='company_id.currency_id', readonly=True)
+    package_price = fields.Monetary('Package Price', currency_field='packaging_currency_id', default=0.0)
