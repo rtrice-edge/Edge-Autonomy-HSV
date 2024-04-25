aa1c1e022887baf455df027b91bee97d60cc07e0
Richard Trice <rtrice@edgeautonomy.io>
2024-04-25 13:49:22 -0700

    ok... trying again!

--- edge_module/models/mrp_production.py
+++ edge_module/models/mrp_production.py
@@ -1,16 +1,22 @@
 from odoo import api, fields, models, _, Command
 
+import logging
+_logger = logging.getLogger(__name__)
+
+
+
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     def action_split(self):
         self._pre_action_split_merge_hook(split=True)
+        _logger.info('Called action_split')
         if len(self) > 1:
             productions = []
             for production in self:
                 # Create a new procurement group for each split MO
                 procurement_group = self.env['procurement.group'].create({'name': production.name})
-                
+                _logger.info('Created procurement group' + str(procurement_group))
                 # Assign the new procurement group to the split MO
                 production_data = {'production_id': production.id, 'procurement_group_id': procurement_group.id}
                 productions.append(Command.create(production_data))
