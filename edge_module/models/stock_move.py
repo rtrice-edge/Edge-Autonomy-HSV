2079dfe8361fae85da112a605d893ff5db96e19c
Richard Trice <rtrice@edgeautonomy.io>
2024-04-25 14:47:03 -0700

    test

--- edge_module/models/stock_move.py
+++ edge_module/models/stock_move.py
@@ -16,7 +16,7 @@ class StockMove(models.Model):
     @api.model
     def create(self, values):
         _logger.info(f"Stock Move Create: {values}")
-        if values.get('picking_type_id') and values['picking_type_id'] == 9:
+        if values.get('picking_type_id') and (values['picking_type_id'] == 9):
             # Generate a procurement_group based on the original receipt
             
 
@@ -31,4 +31,17 @@ class StockMove(models.Model):
                 _logger.info(f"Procurement Group already exists: {procurement_group}")
             values['group_id'] = procurement_group.id
             _logger.info(f"Procurement Group ID assigned: {procurement_group.id}")
+        elif values.get('picking_type_id') and (values['picking_type_id'] == 8):
+            procurement_group_name = values.get('name', False)
+            _logger.info(f"Procurement Group Name: {procurement_group_name}")
+            procurement_group = self.env['procurement.group'].search([('name', '=', procurement_group_name)])
+            if not procurement_group:
+                _logger.info("Procurement Group not found, creating new one...")
+                procurement_group = self.env['procurement.group'].create({'name': procurement_group_name})
+                _logger.info(f"New Procurement Group created: {procurement_group}")
+            else:
+                _logger.info(f"Procurement Group already exists: {procurement_group}")
+            values['group_id'] = procurement_group.id
+            _logger.info(f"Procurement Group ID assigned: {procurement_group.id}")
+             
         return super(StockMove, self).create(values)
