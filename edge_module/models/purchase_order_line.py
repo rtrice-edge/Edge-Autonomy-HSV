9ecf886edfe147b1cb08d6fbdc81f1a1fe2a4247
Jesse Larson <jlarson@edgeautonomy.io>
2024-04-25 09:42:04 -0700

    VERY NICEEEEE

--- edge_module/models/purchase_order_line.py
+++ edge_module/models/purchase_order_line.py
@@ -60,7 +60,7 @@ class PurchaseOrderLine(models.Model):
     package_unit_price = fields.Float(string='Package Unit Price')
 
     packaging_currency_id = fields.Many2one('res.currency', string='Packaging Currency', related='company_id.currency_id', readonly=True)
-    package_price = fields.Monetary('Price of Package', currency_field='packaging_currency_id', default=0.0, compute='_compute_package_price', store=True)
+    package_price = fields.Monetary('Price of Package', currency_field='packaging_currency_id', default=0.0, store=True)
     product_packaging_qty = fields.Integer(string='Number of Packages')
     packaging_qty = fields.Float(related='product_packaging_id.qty')
 
