5eb5df8f75a222ed61df49a32784850cb4d802de
Richard Trice <rtrice@edgeautonomy.io>
2024-05-01 19:30:57 -0700

    t4e3st

--- edge_module/models/demand.py
+++ edge_module/models/demand.py
@@ -154,4 +154,4 @@ class Demand(models.Model):
                             pt.name,
                             pt.type,
                             io."In Inventory",
-                            io."On Order")    """ 
+                            io."On Order")    """ % (self._table))
