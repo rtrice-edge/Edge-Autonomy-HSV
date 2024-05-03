44e35d2517411542d8cbe70729dd87b54247fcfd
Jesse Larson <jlarson@edgeautonomy.io>
2024-05-03 11:21:11 -0700

    tank

--- edge_module/__manifest__.py
+++ edge_module/__manifest__.py
@@ -51,6 +51,7 @@
         'views/vendor/res_partner.xml',
 
         'reports/purchase_order_custom.xml',
+        'reports/ir_actions_report.xml',
 
         'views/purchase_order/purchase_order.xml', 
         
