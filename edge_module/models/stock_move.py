ff1776c7304e4ebdb5864c16d1c8da04f2fa9856
Richard Trice <rtrice@edgeautonomy.io>
2024-05-06 08:04:16 -0700

    Ok... logging the explosion

--- edge_module/models/stock_move.py
+++ edge_module/models/stock_move.py
@@ -1,7 +1,8 @@
 #odoo procurement category
 
-from odoo import models, fields, api
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo import api, Command, fields, models
+from odoo.osv import expression
+from odoo.tools import float_compare, float_round, float_is_zero, OrderedSet
 from datetime import datetime
 import logging
 
@@ -50,3 +51,54 @@ class StockMove(models.Model):
 
              
         return super(StockMove, self).create(values)
+    
+    def action_explode(self):
+        """ Explodes pickings """
+        # in order to explode a move, we must have a picking_type_id on that move because otherwise the move
+        # won't be assigned to a picking and it would be weird to explode a move into several if they aren't
+        # all grouped in the same picking.
+        _logger.info(f"Action Explode: {self}")
+        moves_ids_to_return = OrderedSet()
+        moves_ids_to_unlink = OrderedSet()
+        phantom_moves_vals_list = []
+        for move in self:
+            _logger.info(f"Move: {move}")
+            _logger.info(f"Move Picking Type ID: {move.picking_type_id}")
+            _logger.info(f"Move Production ID: {move.production_id}")   
+            _logger.info(f"Move context is_scrap: {self.env.context.get('is_scrap')}")
+            if (not move.picking_type_id and not self.env.context.get('is_scrap')) or (move.production_id and move.production_id.product_id == move.product_id):
+                _logger.info(f"Move not assigned to a picking or is a production move, skipping")
+                moves_ids_to_return.add(move.id)
+                continue
+            bom = self.env['mrp.bom'].sudo()._bom_find(move.product_id, company_id=move.company_id.id, bom_type='phantom')[move.product_id]
+            _logger.info(f"BOM: {bom}")
+            if not bom:
+                _logger.info(f"No BOM found for product {move.product_id}, skipping")
+                moves_ids_to_return.add(move.id)
+                continue
+            if float_is_zero(move.product_uom_qty, precision_rounding=move.product_uom.rounding):
+                factor = move.product_uom._compute_quantity(move.quantity, bom.product_uom_id) / bom.product_qty
+            else:
+                factor = move.product_uom._compute_quantity(move.product_uom_qty, bom.product_uom_id) / bom.product_qty
+            boms, lines = bom.sudo().explode(move.product_id, factor, picking_type=bom.picking_type_id)
+            for bom_line, line_data in lines:
+                _logger.info(f"BOM Line: {bom_line}")
+                if float_is_zero(move.product_uom_qty, precision_rounding=move.product_uom.rounding) or self.env.context.get('is_scrap'):
+                    phantom_moves_vals_list += move._generate_move_phantom(bom_line, 0, line_data['qty'])
+                else:
+                    phantom_moves_vals_list += move._generate_move_phantom(bom_line, line_data['qty'], 0)
+            # delete the move with original product which is not relevant anymore
+            _logger.info(f"Adding move to unlink: {move}")
+            moves_ids_to_unlink.add(move.id)
+
+        if phantom_moves_vals_list:
+            _logger.info(f"Creating phantom moves: {phantom_moves_vals_list}")
+            phantom_moves = self.env['stock.move'].create(phantom_moves_vals_list)
+            phantom_moves._adjust_procure_method()
+            moves_ids_to_return |= phantom_moves.action_explode().ids
+        move_to_unlink = self.env['stock.move'].browse(moves_ids_to_unlink).sudo()
+        move_to_unlink.quantity = 0
+        move_to_unlink._action_cancel()
+        move_to_unlink.unlink()
+        _logger.info(f"Returning moves: {moves_ids_to_return}")
+        return self.env['stock.move'].browse(moves_ids_to_return)
