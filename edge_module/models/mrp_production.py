e9a06c3f9a21db98ddcc4351e429c3f50e6ddd78
Richard Trice <rtrice@edgeautonomy.io>
2024-05-05 22:19:54 -0700

    tasdfasd

--- edge_module/models/mrp_production.py
+++ edge_module/models/mrp_production.py
@@ -11,12 +11,15 @@ class MrpProduction(models.Model):
     def action_assign(self):
         res = super(MrpProduction, self).action_assign()
         for production in self:
+            _logger.info(f'Checking if pick list should be split for MO {production.name}')
             if production.state == 'confirmed':
+                _logger.info(f'MO {production.name} is confirmed, splitting pick list')
                 production.split_pick_list()
         return res
 
     def split_pick_list(self):
         for production in self:
+            _logger.info(f'Splitting pick list for MO {production.name}')
             # Get the move lines associated with the manufacturing order
             move_lines = production.move_raw_ids
 
@@ -25,6 +28,7 @@ class MrpProduction(models.Model):
 
             # Iterate over the move lines and split them based on their source location
             for move in move_lines:
+                _logger.info(f'Move {move.product_id.name} from {move.location_id.name} to {move.location_dest_id.name}')
                 source_location = move.location_id
                 if source_location not in split_moves:
                     split_moves[source_location] = []
@@ -33,6 +37,7 @@ class MrpProduction(models.Model):
             # Create new pick lists for each split move group
             for location, moves in split_moves.items():
                 # Create a new pick list
+                _logger.info(f'Creating new pick list for location {location.name}')
                 picking = self.env['stock.picking'].create({
                     'picking_type_id': production.picking_type_id.id,
                     'location_id': location.id,
@@ -41,8 +46,10 @@ class MrpProduction(models.Model):
                 })
 
                 # Move the split move lines to the new pick list
+                _logger.info(f'Moving {len(moves)} move lines to new pick list')
                 moves.write({'picking_id': picking.id})
 
             # Update the state of the original pick list if all move lines have been split
             if not move_lines:
+                _logger.info('All move lines have been split, setting original pick list to done')
                 production.picking_ids.write({'state': 'cancel'})
