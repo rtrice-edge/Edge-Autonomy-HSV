151b85ef07deb09796636f4caefa78708b3918f8
Richard Trice <rtrice@edgeautonomy.io>
2024-05-08 14:14:44 -0700

    asdfasdfasdfa

--- edge_module/models/stock_move_line.py
+++ edge_module/models/stock_move_line.py
@@ -14,17 +14,5 @@ class StockMoveLine(models.Model):
 
     original_receipt = fields.Char(string='Original Receipt')
     
-    @api.depends('quant_id')
-    def _compute_quantity(self):
-        """
-        This field represents the sum of the move lines `quantity`. It allows the user to know
-        if there is still work to do.
 
-        We take care of rounding this value to the nearest whole number using the `ceil` function
-        to ensure that the quantity is always rounded up.
-        """
-        super()._compute_quantity()
-        #Changing the quantity to be rounded up
-        for move in self:
-            move.quantity = math.ceil(move.quantity)
             
