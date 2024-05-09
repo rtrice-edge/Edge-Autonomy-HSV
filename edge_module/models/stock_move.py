dbfdc95b3d7805677355e1b9831c7a5e5ba00378
Richard Trice <rtrice@edgeautonomy.io>
2024-05-09 10:42:56 -0700

    asdfasdf

--- edge_module/models/stock_move.py
+++ edge_module/models/stock_move.py
@@ -17,52 +17,52 @@ class StockMove(models.Model):
 
     #maybe maybe maybe
 
-    @api.model
-    def create(self, values):
+    # @api.model
+    # def create(self, values):
         
-        if values.get('picking_type_id') and (values['picking_type_id'] == 9):
-            # Generate a procurement_group based on the original receipt
+    #     if values.get('picking_type_id') and (values['picking_type_id'] == 9):
+    #         # Generate a procurement_group based on the original receipt
             
 
-            procurement_group_name = values.get('origin', False)
+    #         procurement_group_name = values.get('origin', False)
            
-            procurement_group = self.env['procurement.group'].search([('name', '=', procurement_group_name)])
-            if not procurement_group:
-                _logger.info("Procurement Group not found, creating new one...")
-                procurement_group = self.env['procurement.group'].create({'name': procurement_group_name})
-                _logger.info(f"New Procurement Group created: {procurement_group}")
-            values['group_id'] = procurement_group.id
+    #         procurement_group = self.env['procurement.group'].search([('name', '=', procurement_group_name)])
+    #         if not procurement_group:
+    #             _logger.info("Procurement Group not found, creating new one...")
+    #             procurement_group = self.env['procurement.group'].create({'name': procurement_group_name})
+    #             _logger.info(f"New Procurement Group created: {procurement_group}")
+    #         values['group_id'] = procurement_group.id
 
             
             
-        elif values.get('picking_type_id') and (values['picking_type_id'] in [6,7]):
-                        # I call the real Create method and then adjust the values after.  
+    #     elif values.get('picking_type_id') and (values['picking_type_id'] in [6,7]):
+    #                     # I call the real Create method and then adjust the values after.  
 
-            mymove = super(StockMove, self).create(values)
-            _logger.info(f"Stock Move Created: {mymove}")
-            # log all the values in the stock move
-            _logger.info(f"Stock Move Values: {mymove.name} {mymove.origin} {mymove.group_id} {mymove.picking_type_id} {mymove.product_id} {mymove.product_uom_qty} {mymove.product_uom} {mymove.location_id} {mymove.location_dest_id} ")
-            procurement_group_name = values['origin']
-            procurement_group = self.env['procurement.group'].search([('name', '=', procurement_group_name)])
-            if not procurement_group:
-                _logger.info("Procurement Group not found, creating new one...")
-                procurement_group = self.env['procurement.group'].create({'name': procurement_group_name})
-                _logger.info(f"New Procurement Group created: {procurement_group}")
-            # The following code sets both the group id to keep things separate, and the location and destination to the correct values
-            mymove.group_id = procurement_group.id
-            if (mymove.picking_type_id.id == 6):
-                _logger.info("Changing location to destination to 15")
-                mymove.location_dest_id = 15
-                _logger.info("Bomb line notes are" + str(mymove.bom_line_id.notes))
-                mymove.bom_line_notes = mymove.bom_line_id.notes 
-            if (mymove.picking_type_id.id == 7):
-                _logger.info("Changing location to location to 18")
-                mymove.location_id = 18
-            _logger.info("My location and destinations are" + str(mymove.location_id) + " " + str(mymove.location_dest_id))
-            return mymove
+    #         mymove = super(StockMove, self).create(values)
+    #         _logger.info(f"Stock Move Created: {mymove}")
+    #         # log all the values in the stock move
+    #         _logger.info(f"Stock Move Values: {mymove.name} {mymove.origin} {mymove.group_id} {mymove.picking_type_id} {mymove.product_id} {mymove.product_uom_qty} {mymove.product_uom} {mymove.location_id} {mymove.location_dest_id} ")
+    #         procurement_group_name = values['origin']
+    #         procurement_group = self.env['procurement.group'].search([('name', '=', procurement_group_name)])
+    #         if not procurement_group:
+    #             _logger.info("Procurement Group not found, creating new one...")
+    #             procurement_group = self.env['procurement.group'].create({'name': procurement_group_name})
+    #             _logger.info(f"New Procurement Group created: {procurement_group}")
+    #         # The following code sets both the group id to keep things separate, and the location and destination to the correct values
+    #         mymove.group_id = procurement_group.id
+    #         if (mymove.picking_type_id.id == 6):
+    #             _logger.info("Changing location to destination to 15")
+    #             mymove.location_dest_id = 15
+    #             _logger.info("Bomb line notes are" + str(mymove.bom_line_id.notes))
+    #             mymove.bom_line_notes = mymove.bom_line_id.notes 
+    #         if (mymove.picking_type_id.id == 7):
+    #             _logger.info("Changing location to location to 18")
+    #             mymove.location_id = 18
+    #         _logger.info("My location and destinations are" + str(mymove.location_id) + " " + str(mymove.location_dest_id))
+    #         return mymove
 
              
-        return super(StockMove, self).create(values)
+    #     return super(StockMove, self).create(values)
     
     # def _compute_quantity(self):
     #     """
@@ -77,10 +77,10 @@ class StockMove(models.Model):
     #     for move in self:
     #         move.quantity = math.ceil(move.quantity)
             
-    def _prepare_mo_qty(self, quantity):
-        res = super(StockMove, self)._prepare_mo_qty(quantity)
-        _logger.info(f"I called prepare_mo_qty: {res}")
-        _logger.info(f"self.picking_type_id.id: {self.picking_type_id.id}")
-        if (self.picking_type_id.id == 6):
-            res['location_id'] = self.location_id.id  # Use the specified location_id instead of the production location
-        return res
+    # def _prepare_mo_qty(self, quantity):
+    #     res = super(StockMove, self)._prepare_mo_qty(quantity)
+    #     _logger.info(f"I called prepare_mo_qty: {res}")
+    #     _logger.info(f"self.picking_type_id.id: {self.picking_type_id.id}")
+    #     if (self.picking_type_id.id == 6):
+    #         res['location_id'] = self.location_id.id  # Use the specified location_id instead of the production location
+    #     return res
