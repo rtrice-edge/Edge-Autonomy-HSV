0ab1e75b1c4d78583631e7027f5f38a12252ba38
Richard Trice <rtrice@edgeautonomy.io>
2024-05-11 09:18:00 -0700

    Adjust demand model

--- edge_module/models/demand.py
+++ edge_module/models/demand.py
@@ -13,6 +13,7 @@ class Demand(models.Model):
     component_code = fields.Char(string='Component Code', required=False, readonly=True)
     component_name = fields.Char(string='Component Name', required=False, readonly=True)
     is_storable = fields.Boolean(string='Consumable?', required=False, readonly=True)
+    has_bom = fields.Boolean(string='Has BOM?', required=False, readonly=True)
     in_stock = fields.Float(string='In Stock', required=False, readonly=True)
     on_order = fields.Float(string='On Order', required=False, readonly=True)
     current_month = datetime.now().month
@@ -82,134 +83,145 @@ class Demand(models.Model):
         # tools.drop_view_if_exists(self.env.cr, self._table)
         self._cr.execute("""
                          CREATE OR REPLACE VIEW demand_model AS (
-                         WITH inventory_on_order AS (
-                            SELECT
-                                pp.id AS product_id,
-                                COALESCE(inventory.on_hand_quantity, 0) AS "In Inventory",
-                                COALESCE(on_order.on_order_quantity, 0) AS "On Order"
-                            FROM
-                                product_product pp
-                            LEFT JOIN
-                                (
-                                    SELECT
-                                        pp.id AS product_id,
-                                        SUM(sq.quantity) AS on_hand_quantity
-                                    FROM
-                                        stock_quant sq
-                                    JOIN
-                                        stock_location sl ON sq.location_id = sl.id
-                                    JOIN
-                                        product_product pp ON sq.product_id = pp.id
-                                    WHERE
-                                        sl.complete_name NOT LIKE 'Virtual Locations/%' -- Exclude virtual locations
-                                        AND sl.complete_name NOT LIKE 'Partners/%' -- Exclude partner locations
-                                    GROUP BY
-                                        pp.id
-                                ) inventory ON pp.id = inventory.product_id
-                            LEFT JOIN
-                                (
-                                    SELECT
-                                        pp.id AS product_id,
-                                        SUM(pol.product_qty - pol.qty_received) AS on_order_quantity
-                                    FROM
-                                        purchase_order_line pol
-                                    JOIN
-                                        product_product pp ON pol.product_id = pp.id
-                                    JOIN
-                                        purchase_order po ON pol.order_id = po.id
-                                    WHERE
-                                        po.state IN ('draft', 'sent', 'to approve', 'purchase', 'done') -- Include 'done' state
-                                    GROUP BY
-                                        pp.id
-                                ) on_order ON pp.id = on_order.product_id
-                        ),
-                        months AS (
-                            SELECT DISTINCT DATE_TRUNC('month', mo.date_start)::DATE AS month
-                            FROM mrp_production mo
-                            WHERE mo.state IN ('draft', 'confirmed', 'progress')
-                            UNION
-                            SELECT GENERATE_SERIES(
-                                (SELECT MIN(DATE_TRUNC('month', date_start)::DATE) FROM mrp_production WHERE state IN ('draft', 'confirmed', 'progress')),
-                                (SELECT MIN(DATE_TRUNC('month', date_start)::DATE) FROM mrp_production WHERE state IN ('draft', 'confirmed', 'progress')) + INTERVAL '7 months',
-                                INTERVAL '1 month'
-                            )
-                        )
-                        SELECT
-                            mobl.product_id AS id,
-                            mobl.product_id AS product_id,
-                            p.default_code AS component_code,
-                            pt.name->>'en_US' AS component_name,
-                            CASE WHEN pt.type = 'product' THEN true ELSE false END AS is_storable,
-                            COALESCE(io."In Inventory", 0) AS "in_stock",
-                            COALESCE(io."On Order", 0) AS "on_order",
-                            SUM(
-                                CASE
-                                    WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
-                                    ELSE mobl.product_qty * mo.product_qty
-                                END
-                            ) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE <= m.month) AS month_1,
-                            SUM(
-                                CASE
-                                    WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
-                                    ELSE mobl.product_qty * mo.product_qty
-                                END
-                            ) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '1 month') AS month_2,
-                            SUM(
-                                CASE
-                                    WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
-                                    ELSE mobl.product_qty * mo.product_qty
-                                END
-                            ) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '2 months') AS month_3,
-                            SUM(
-                                CASE
-                                    WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
-                                    ELSE mobl.product_qty * mo.product_qty
-                                END
-                            ) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '3 months') AS month_4,
-                            SUM(
-                                CASE
-                                    WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
-                                    ELSE mobl.product_qty * mo.product_qty
-                                END
-                            ) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '4 months') AS month_5,
-                            SUM(
-                                CASE
-                                    WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
-                                    ELSE mobl.product_qty * mo.product_qty
-                                END
-                            ) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '5 months') AS month_6,
-                            SUM(
-                                CASE
-                                    WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
-                                    ELSE mobl.product_qty * mo.product_qty
-                                END
-                            ) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '6 months') AS month_7,
-                            SUM(
-                                CASE
-                                    WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
-                                    ELSE mobl.product_qty * mo.product_qty
-                                END
-                            ) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '7 months') AS month_8
-                        FROM
-                            months m
-                        CROSS JOIN
-                            mrp_bom_line mobl
-                        JOIN
-                            mrp_bom mb ON mobl.bom_id = mb.id
-                        JOIN
-                            mrp_production mo ON mb.id = mo.bom_id AND mo.state IN ('draft', 'confirmed', 'progress')
-                        JOIN
-                            product_product p ON mobl.product_id = p.id
-                        JOIN
-                            product_template pt ON p.product_tmpl_id = pt.id
-                        JOIN
-                            uom_uom uom ON mobl.product_uom_id = uom.id
-                        LEFT JOIN
-                            inventory_on_order io ON p.id = io.product_id
-                        GROUP BY
-                            mobl.product_id,
-                            p.default_code,
-                            pt.name,
-                            pt.type,
-                            io."In Inventory",
-                            io."On Order")    """ )  #random comment
+WITH inventory_on_order AS (
+SELECT
+pp.id AS product_id,
+COALESCE(inventory.on_hand_quantity, 0) AS "In Inventory",
+COALESCE(on_order.on_order_quantity, 0) AS "On Order"
+FROM
+product_product pp
+LEFT JOIN
+(
+SELECT
+pp.id AS product_id,
+SUM(sq.quantity) AS on_hand_quantity
+FROM
+stock_quant sq
+JOIN
+stock_location sl ON sq.location_id = sl.id
+JOIN
+product_product pp ON sq.product_id = pp.id
+WHERE
+sl.complete_name NOT LIKE 'Virtual Locations/%' -- Exclude virtual locations
+AND sl.complete_name NOT LIKE 'Partners/%' -- Exclude partner locations
+GROUP BY
+pp.id
+) inventory ON pp.id = inventory.product_id
+LEFT JOIN
+(
+SELECT
+pp.id AS product_id,
+SUM(pol.product_qty - pol.qty_received) AS on_order_quantity
+FROM
+purchase_order_line pol
+JOIN
+product_product pp ON pol.product_id = pp.id
+JOIN
+purchase_order po ON pol.order_id = po.id
+WHERE
+po.state IN ('draft', 'sent', 'to approve', 'purchase', 'done') -- Include 'done' state
+GROUP BY
+pp.id
+) on_order ON pp.id = on_order.product_id
+),
+months AS (
+SELECT DISTINCT DATE_TRUNC('month', mo.date_start)::DATE AS month
+FROM mrp_production mo
+WHERE mo.state IN ('draft', 'confirmed', 'progress')
+UNION
+SELECT GENERATE_SERIES(
+(SELECT MIN(DATE_TRUNC('month', date_start)::DATE) FROM mrp_production WHERE state IN ('draft', 'confirmed', 'progress')),
+(SELECT MIN(DATE_TRUNC('month', date_start)::DATE) FROM mrp_production WHERE state IN ('draft', 'confirmed', 'progress')) + INTERVAL '7 months',
+INTERVAL '1 month'
+)
+)
+SELECT
+mobl.product_id AS id,
+mobl.product_id AS product_id,
+p.default_code AS component_code,
+pt.name->>'en_US' AS component_name,
+CASE WHEN pt.type = 'product' THEN true ELSE false END AS is_storable,
+COALESCE(io."In Inventory", 0) AS "in_stock",
+COALESCE(io."On Order", 0) AS "on_order",
+CASE WHEN EXISTS (
+SELECT 1
+FROM mrp_bom mb
+WHERE mb.product_tmpl_id = pt.id
+) THEN true ELSE false END AS has_bom,
+SUM(
+CASE
+WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
+ELSE mobl.product_qty * mo.product_qty
+END
+) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE <= m.month) AS month_1,
+SUM(
+CASE
+WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
+ELSE mobl.product_qty * mo.product_qty
+END
+) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '1 month') AS month_2,
+SUM(
+CASE
+WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
+ELSE mobl.product_qty * mo.product_qty
+END
+) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '2 months') AS month_3,
+SUM(
+CASE
+WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
+ELSE mobl.product_qty * mo.product_qty
+END
+) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '3 months') AS month_4,
+SUM(
+CASE
+WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
+ELSE mobl.product_qty * mo.product_qty
+END
+) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '4 months') AS month_5,
+SUM(
+CASE
+WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
+ELSE mobl.product_qty * mo.product_qty
+END
+) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '5 months') AS month_6,
+SUM(
+CASE
+WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
+ELSE mobl.product_qty * mo.product_qty
+END
+) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '6 months') AS month_7,
+SUM(
+CASE
+WHEN uom.name->>'en_US' = 'in' THEN mobl.product_qty * mo.product_qty / 12
+ELSE mobl.product_qty * mo.product_qty
+END
+) FILTER (WHERE DATE_TRUNC('month', mo.date_start)::DATE = m.month + INTERVAL '7 months') AS month_8
+FROM
+months m
+CROSS JOIN
+mrp_bom_line mobl
+JOIN
+mrp_bom mb ON mobl.bom_id = mb.id
+JOIN
+mrp_production mo ON mb.id = mo.bom_id AND mo.state IN ('draft', 'confirmed', 'progress')
+JOIN
+product_product p ON mobl.product_id = p.id
+JOIN
+product_template pt ON p.product_tmpl_id = pt.id
+JOIN
+uom_uom uom ON mobl.product_uom_id = uom.id
+LEFT JOIN
+inventory_on_order io ON p.id = io.product_id
+GROUP BY
+mobl.product_id,
+p.default_code,
+pt.name,
+pt.type,
+io."In Inventory",
+io."On Order",
+CASE WHEN EXISTS (
+SELECT 1
+FROM mrp_bom mb
+WHERE mb.product_tmpl_id = pt.id
+) THEN true ELSE false END
+)   """ )  #random comment
