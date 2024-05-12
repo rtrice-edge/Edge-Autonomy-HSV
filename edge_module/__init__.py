7c15032cde154735b50775cfb4009c8d554ab0a4
Richard Trice <rtrice@edgeautonomy.io>
2024-05-12 10:45:03 -0700

    maybe this all along

--- edge_module/__init__.py
+++ edge_module/__init__.py
@@ -1,2 +1,11 @@
 from . import models
 from . import controllers
+from odoo import SUPERUSER_ID, api
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['ir.ui.view'].clear_caches()
+
+def uninstall_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['ir.ui.view'].clear_caches()
