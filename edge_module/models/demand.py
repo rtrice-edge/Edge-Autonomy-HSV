faa48b4976bd1ec9eab257c25d31e38b6b4fb0eb
Richard Trice <rtrice@edgeautonomy.io>
2024-05-12 19:17:07 -0700

    asdf

--- edge_module/models/demand.py
+++ edge_module/models/demand.py
@@ -64,7 +64,13 @@ class Demand(models.Model):
             'target': 'new',
         }
 
-    
+    def action_view_purchase_orders(self):
+        self.ensure_one()
+        action = self.env["ir.actions.actions"]._for_xml_id("purchase.purchase_rfq")
+        action['domain'] = [('state', 'in', ['draft', 'sent', 'to approve']), ('product_id', '=', self.product_id.id)]
+        action['context'] = {'search_default_product_id': self.product_id.id, 'default_product_id': self.product_id.id}
+        return action
+
     @api.depends('in_stock', 'on_order')
     def _compute_values(self):
         for record in self:
