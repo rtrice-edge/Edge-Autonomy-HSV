8fc361285620c82efe5062be475c375554cd350b
Richard Trice <rtrice@edgeautonomy.io>
2024-05-21 09:04:08 -0700

    test

--- edge_module/models/mrp_workorder.py
+++ edge_module/models/mrp_workorder.py
@@ -3,22 +3,10 @@ from odoo import api, fields, models
 class MrpWorkorder(models.Model):
     _inherit = 'mrp.workorder'
 
-    quality_check_ids = fields.One2many('quality.check', 'workorder_id', string='Quality Checks')
-    # quality_check_count = fields.Integer(compute='_compute_quality_check_count', string='Quality Check Count')
-    # invisible_button = fields.Boolean(compute='_compute_invisible_button')
+    quality_check_ids = fields.One2many('quality.check', compute='_compute_quality_check_ids', string='Quality Checks')
 
-    # @api.depends('quality_check_ids')
-    # def _compute_quality_check_count(self):
-    #     for workorder in self:
-    #         workorder.quality_check_count = len(workorder.quality_check_ids)
-
-    # @api.depends('quality_check_ids')
-    # def _compute_invisible_button(self):
-    #     for workorder in self:
-    #         workorder.invisible_button = not bool(workorder.quality_check_ids)
-
-    # def action_view_quality_checks(self):
-    #     self.ensure_one()
-    #     action = self.env.ref('quality_control.quality_check_action_main').read()[0]
-    #     action['domain'] = [('id', 'in', self.quality_check_ids.ids)]
-    #     return action
+    @api.depends('id')
+    def _compute_quality_check_ids(self):
+        for workorder in self:
+            quality_checks = self.env['quality.check'].search([('workorder_id', '=', workorder.id)])
+            workorder.quality_check_ids = quality_checks
