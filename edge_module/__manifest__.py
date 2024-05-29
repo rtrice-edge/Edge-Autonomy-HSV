63ad50072fd1a380bbdb8c84c3979c72d74fbb02
jlars0n <[jlarson@edgeautonomy.io]>
2024-05-29 14:06:11 -0700

    a thing

--- edge_module/__manifest__.py
+++ edge_module/__manifest__.py
@@ -73,8 +73,6 @@
 
         'reports/mrp_report_mrporder.xml',
 
-        'reports/address_layout_report.xml',
-
         #'reports/company_details_report.xml',
 
         'reports/report_delivery_document_custom.xml',
