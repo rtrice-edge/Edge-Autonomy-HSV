420a8706bb0ada8ff05d6c1ca384cac537df07c6
Richard Trice <rtrice@edgeautonomy.io>
2024-06-04 15:14:33 -0700

    ugh

--- edge_module/models/quality.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class QualityCheck(models.Model):
-    _inherit = 'quality.check'
-
-    worksheet_document = fields.Binary(related='point_id.worksheet_document', readonly=True)
