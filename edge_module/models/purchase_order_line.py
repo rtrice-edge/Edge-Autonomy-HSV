49a97fb28bed47d5ab4c4db76205a22239ddd54b
Richard Trice <rtrice@edgeautonomy.io>
2024-06-14 11:41:58 -0700

    leaving them in for a hot min while I update

--- edge_module/models/purchase_order_line.py
+++ edge_module/models/purchase_order_line.py
@@ -12,39 +12,39 @@ class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
 
-    # costobjective = fields.Selection([
-    #     ('direct', 'Direct'),
-    #     ('g&a', 'G&A'),
-    #     ('engovh', 'Eng OVH'),
-    #     ('manovh', 'Man OVH'),
-    #     ('ir&d', 'IR&D'),
-    #     ('b&p', 'B&P')
-    # ], string='Cost Objective', required=False ,default='direct')
+    costobjective = fields.Selection([
+        ('direct', 'Direct'),
+        ('g&a', 'G&A'),
+        ('engovh', 'Eng OVH'),
+        ('manovh', 'Man OVH'),
+        ('ir&d', 'IR&D'),
+        ('b&p', 'B&P')
+    ], string='Cost Objective', required=False ,default='direct')
 
 
-    # expensetype = fields.Selection([
-    #     ('engineeringmaterials ', 'Engineering Materials '),
-    #     ('electronicsmaterials ', 'Electronics Materials '),
-    #     ('inventory/procurementmaterials ', 'Inventory/Procurement Materials '),
-    #     ('composites/assemblymaterials ', 'Composites/Assembly Materials '),
-    #     ('groundsupportmaterials ', 'Ground Support Materials '),
-    #     ('subklabor-engineering ', 'Subk Labor-Engineering '),
-    #     ('subklabor-composites/assembly ', 'Subk Labor-Composites/Assembly '),
-    #     ('packing,postage,andfreight ', 'Packing, Postage, and Freight '),
-    #     ('smalltooling ', 'Small Tooling '),
-    #     ('itservices ', 'IT Services '),
-    #     ('ithardware/peripherals ', 'IT Hardware/Peripherals '),
-    #     ('itsoftware ', 'IT Software '),
-    #     ('professionalservices/consultants ', 'Professional Services/Consultants '),
-    #     ('supplies ', 'Supplies '),
-    #     ('janitorial ', 'Janitorial '),
-    #     ('repairs&maintenance ', 'Repairs & Maintenance '),
-    #     ('equipmentrental ', 'Equipment Rental '),
-    #     ('flighttesting ', 'Flight Testing '),
-    #     ('smalltestequipment ', 'Small Test Equipment '),
-    #     ('wastedisposal ', 'Waste Disposal '),
-    #     ('safety ', 'Safety '),
-    #], string='Expense Type', required=False, default='inventory/procurementmaterials ')
+    expensetype = fields.Selection([
+        ('engineeringmaterials ', 'Engineering Materials '),
+        ('electronicsmaterials ', 'Electronics Materials '),
+        ('inventory/procurementmaterials ', 'Inventory/Procurement Materials '),
+        ('composites/assemblymaterials ', 'Composites/Assembly Materials '),
+        ('groundsupportmaterials ', 'Ground Support Materials '),
+        ('subklabor-engineering ', 'Subk Labor-Engineering '),
+        ('subklabor-composites/assembly ', 'Subk Labor-Composites/Assembly '),
+        ('packing,postage,andfreight ', 'Packing, Postage, and Freight '),
+        ('smalltooling ', 'Small Tooling '),
+        ('itservices ', 'IT Services '),
+        ('ithardware/peripherals ', 'IT Hardware/Peripherals '),
+        ('itsoftware ', 'IT Software '),
+        ('professionalservices/consultants ', 'Professional Services/Consultants '),
+        ('supplies ', 'Supplies '),
+        ('janitorial ', 'Janitorial '),
+        ('repairs&maintenance ', 'Repairs & Maintenance '),
+        ('equipmentrental ', 'Equipment Rental '),
+        ('flighttesting ', 'Flight Testing '),
+        ('smalltestequipment ', 'Small Test Equipment '),
+        ('wastedisposal ', 'Waste Disposal '),
+        ('safety ', 'Safety '),
+    ], string='Expense Type', required=False, default='inventory/procurementmaterials ')
 
 
     cost_objective = fields.Selection(
