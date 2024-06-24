f722c137c8dbff32f1f0826083634a155fac4f02
Richard Trice <rtrice@edgeautonomy.io>
2024-06-24 14:17:33 -0700

    purchase amendment addition

--- edge_module/models/purchase_order.py
+++ edge_module/models/purchase_order.py
@@ -24,7 +24,12 @@ class PurchaseOrder(models.Model):
     edge_recipient_new = fields.Many2one('hr.employee', string='Internal Recipient', help="This is where you select the person who the package is to be delivered to when it enters the facility. This defailts as the person who created the purchase request.")
 
     #purchase_contact = fields.Many2one('hr.employee', string='Edge Contact')
-
+    revision = fields.Integer(string='Amendment Count',copy=False)
+    state = fields.Selection(selection_add=[('amendment', 'Amendment')])
+    amendment_name = fields.Char('Order Reference', copy=True, readonly=True)
+    current_amendment_id = fields.Many2one('purchase.order', 'Current Amendment', readonly=True)
+    old_amendment_ids = fields.One2many('purchase.order', 'current_amendment_id', 'Old Amendment', readonly=True,
+                                        context={'active_test': False})
 
  
     @api.model
@@ -75,3 +80,74 @@ class PurchaseOrder(models.Model):
     #                     _logger.info(order_line)
     #                     order_line['name'] = line.product_description_variants
     #     return super(PurchaseOrder, self).create(vals)
+
+
+
+    @api.model
+    def create(self, vals):
+        res = super(PurchaseOrder, self).create(vals)
+        if res.name:
+            res.amendment_name = res.name
+        return res
+
+    def button_draft(self):
+        orders = self.filtered(lambda s: s.state in ['cancel', 'sent', 'amendment'])
+        orders.write({
+            'state': 'draft',
+        })
+
+    def create_amendment(self):
+        self.ensure_one()
+        # Assign Form view before amendment
+        view_ref = self.env['ir.model.data'].check_object_reference('purchase', 'purchase_order_form')
+        view_id = view_ref and view_ref[1] or False,
+        self.with_context(new_purchase_amendment=True).copy()
+        self.write({'state': 'draft'})
+        self.order_line.write({'state': 'draft'})
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Purchase Order',
+            'res_model': 'purchase.order',
+            'res_id': self.id,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': view_id,
+            'target': 'current',
+            'nodestroy': True,
+        }
+
+    @api.returns('self', lambda value: value.id)
+    def copy(self, defaults=None):
+        if not defaults:
+            defaults = {}
+        if self.env.context.get('new_purchase_amendment'):
+            prev_name = self.name
+            revno = self.revision
+            amendment_ids = self.old_amendment_ids.ids
+            # Assign default values for views
+            self.write({'revision': revno + 1, 'name': '%s-%02d' % (self.amendment_name, revno + 1)})
+            defaults.update({'name': prev_name, 'revision': revno, 'state': 'cancel', 'invoice_count': 0,
+                             'current_amendment_id': self.id, 'amendment_name': self.amendment_name,
+                             })
+        return super(PurchaseOrder, self).copy(defaults)
+
+    def button_amend(self):
+        prev_name = self.name
+        for purchase in self:
+            for picking_loop in purchase.picking_ids:
+                if picking_loop.state == 'done':
+                    raise UserError(
+                        f"Unable to amend purchase order {prev_name}, as some receptions have already been processed."
+                    )
+                else:
+                    picking_loop.filtered(lambda r: r.state != 'cancel').action_cancel()
+            for invoice_loop in purchase.invoice_ids:
+                if invoice_loop.state != 'draft':
+                    raise UserError(
+                        'Unable to amend this purchase order, You must first cancel all Supplier Invoices related to '
+                        'this purchase order.')
+                else:
+                    invoice_loop.filtered(lambda r: r.state != 'cancel').action_invoice_cancel()
+        self.button_draft()
+        self.create_amendment()
+        self.write({'state': 'amendment'})
