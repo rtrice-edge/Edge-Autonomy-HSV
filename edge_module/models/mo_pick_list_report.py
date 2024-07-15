fcadd21ba43e7b691514694ed96d49453a76e1c3
Richard Trice <rtrice@edgeautonomy.io>
2024-07-15 14:54:53 -0700

    again... it swears its good now

--- edge_module/models/mo_pick_list_report.py
+++ edge_module/models/mo_pick_list_report.py
@@ -1,31 +1,26 @@
-from odoo import api, models
+from odoo import models, api
 
 class ReportMOPickList(models.AbstractModel):
     _name = 'report.edge_module.report_mo_pick_list'
     _description = 'MO Pick List Report'
 
     @api.model
+    def _get_available_locations(self, move):
+        return self.env['stock.quant'].search([
+            ('product_id', '=', move.product_id.id),
+            ('location_id.usage', '=', 'internal'),
+            ('quantity', '>', 0),
+            ('reserved_quantity', '<', 'quantity')
+        ]).mapped('location_id')
+
+    @api.model
     def _get_report_values(self, docids, data=None):
         docs = self.env['mrp.production'].browse(docids)
-        
-        def get_available_locations(move):
-            return self.env['stock.quant'].search([
-                ('product_id', '=', move.product_id.id),
-                ('location_id.usage', '=', 'internal'),
-                ('quantity', '>', 0),
-                ('reserved_quantity', '<', 'quantity')
-            ]).mapped('location_id')
-
-        available_locations = {}
-        for doc in docs:
-            for move in doc.move_raw_ids:
-                available_locations[move.id] = get_available_locations(move)
 
         return {
             'doc_ids': docids,
             'doc_model': 'mrp.production',
             'docs': docs,
             'data': data,
-            'available_locations': available_locations,
-            'get_available_locations': lambda move_id: available_locations.get(move_id, []),
+            'get_available_locations': self._get_available_locations,
         }
