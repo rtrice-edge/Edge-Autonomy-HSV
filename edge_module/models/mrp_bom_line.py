1577ccb54e9721ba6e2c0943dbc0e4058194763d
Richard Trice <rtrice@edgeautonomy.io>
2024-08-07 12:54:29 -0700

    ah push it.

--- edge_module/models/mrp_bom_line.py
+++ edge_module/models/mrp_bom_line.py
@@ -4,3 +4,4 @@ class MrpBom(models.Model):
     _inherit = 'mrp.bom.line'
 
     notes = fields.Text(string='Notes')
+    product_type = fields.Selection(related='product_id.type', string='Component Type', readonly=True)
