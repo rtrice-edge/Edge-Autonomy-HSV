c5948ce8175869247d045f5a208f3682c9b5edef
Richard Trice <rtrice@edgeautonomy.io>
2024-08-08 08:33:59 -0700

    ping x2

--- edge_module/models/helpdesk_ticket.py
+++ edge_module/models/helpdesk_ticket.py
@@ -4,6 +4,7 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     purchase_order_id = fields.Many2one('purchase.order', string='Purchase Order')
+    purchase_order_line_id = fields.Many2one('purchase.order.line', string='Purchase Order Line', domain="[('order_id', '=', purchase_order_id)]")
     subtype = fields.Selection([
         ('no_cofc', 'No CofC'),
         ('overshipped', 'Overshipped'),
@@ -11,3 +12,7 @@ class HelpdeskTicket(models.Model):
         ('short_shipped', 'Short Shipped'),
         ('other', 'Other')
     ], string='Subtype')
+
+    @api.onchange('purchase_order_id')
+    def _onchange_purchase_order_id(self):
+        self.purchase_order_line_id = False
