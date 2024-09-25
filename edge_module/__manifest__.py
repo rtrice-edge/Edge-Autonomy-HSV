c67a79dc89289e7c6ae85e15afec492061737db6
Richard Trice <rtrice@edgeautonomy.io>
2024-09-25 10:52:49 -0700

    test

--- edge_module/__manifest__.py
+++ edge_module/__manifest__.py
@@ -87,7 +87,7 @@
         #'reports/company_details_report.xml',
 
         'reports/report_delivery_document_custom.xml',
-        'reports/delivery_order.xml',
+        'reports/production_order.xml',
         'reports/report_purchaseorder_document.xml',
         'reports/purchase_open_lines_report_views.xml',
         'reports/request_for_quotation.xml'
