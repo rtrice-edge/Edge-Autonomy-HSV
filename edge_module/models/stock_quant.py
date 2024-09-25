88ec820595804edcc3d6d6c6cb000fbea5080bae
Richard Trice <rtrice@edgeautonomy.io>
2024-09-25 13:57:47 -0700

    ok maybe.

--- edge_module/models/stock_quant.py
+++ edge_module/models/stock_quant.py
@@ -59,6 +59,7 @@ class StockQuant(models.Model):
                     'cycle_count_id': cycle_count.id,
                     'product_id': quant.product_id.id,
                     'expected_quantity': quant.quantity,  # Changed from inventory_quantity_auto
+                    'lot_id': quant.lot_id.id,
                     'actual_quantity': quant.inventory_quantity,
                     'actual_count_date': fields.Datetime.now(),
                     'user_id': self.env.user.id,
