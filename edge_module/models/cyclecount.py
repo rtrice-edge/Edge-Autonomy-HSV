6d883c4505ab6f5e4226709fab63ccb02a8381ac
Richard Trice <rtrice@edgeautonomy.io>
2024-09-30 13:21:51 -0700

    changes

--- edge_module/models/cyclecount.py
+++ edge_module/models/cyclecount.py
@@ -112,7 +112,7 @@ class CycleCountLog(models.Model):
 
     cycle_count_id = fields.Many2one('inventory.cycle.count', string='Cycle Count', required=True)
     product_id = fields.Many2one('product.product', string='Product', required=True)
-    lot_id = fields.Many2one('stock.production.lot', string='Lot/Serial Number')
+    lot_id = fields.Many2one('stock.lot', string='Lot/Serial Number')
     expected_quantity = fields.Float(string='Expected Quantity')
     actual_quantity = fields.Float(string='Actual Quantity')
     location_id = fields.Many2one('stock.location', string='Location',required=True)
