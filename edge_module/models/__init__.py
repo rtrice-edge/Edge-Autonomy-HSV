3145f8770b8e4ce4f9aaeaffd9160be247674fc7
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2024-10-11 16:21:00 -0700

    attempt number eleventeen

--- edge_module/models/__init__.py
+++ edge_module/models/__init__.py
@@ -2,7 +2,6 @@
 
 from . import mrp_bom_line, product_template, project_project, project_table, purchase_order_line, purchase_order, res_partner
 from . import sale_order_line, stock_move_line, stock_move, stock_picking, stock_quant
-from . import trade_show_shipment, purchase_requisition, mrp_production, demand, componentmoview, mrp_workorder, quality_check
+from . import trade_show_shipment, purchase_requisition, mrp_production, demand, componentmoview, mrp_workorder, quality_check, mrp_production_summary
 from . import accountmapping, report_mrp_order_detailed, helpdesk_ticket, purchase_open_lines_report, quality_alert
 from . import jamisbill, accountmove, cyclecount
-from . import mrp_production_summary
