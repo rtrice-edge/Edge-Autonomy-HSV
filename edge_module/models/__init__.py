3fb3549a60e3171cba92d383fbe4bd41eabc4688
Richard Trice <rtrice@edgeautonomy.io>
2024-10-29 15:09:36 -0700

    test

--- edge_module/models/__init__.py
+++ edge_module/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from . import mrp_bom_line, product_template, project_project, project_table, purchase_order_line, purchase_order, res_partner
+from . import jobs, mrp_bom_line, product_template, project_project, project_table, purchase_order_line, purchase_order, res_partner
 from . import sale_order_line, stock_move_line, stock_move, stock_picking, stock_quant
 from . import trade_show_shipment, purchase_requisition, mrp_production, demand, componentmoview, mrp_workorder, quality_check, mrp_production_summary
 from . import accountmapping, report_mrp_order_detailed, helpdesk_ticket, purchase_open_lines_report, quality_alert
-from . import jamisbill, accountmove, cyclecount, jobs
+from . import jamisbill, accountmove, cyclecount
