296def76f224506c89f776d5dfb8747bac60a714
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2024-11-12 16:22:52 -0800

    ok this

--- edge_module/models/__init__.py
+++ edge_module/models/__init__.py
@@ -4,4 +4,4 @@ from . import jobs, mrp_bom_line, product_template, project_project, project_tab
 from . import sale_order_line, stock_move_line, stock_move, stock_picking, stock_quant
 from . import trade_show_shipment, purchase_requisition, mrp_production, demand, componentmoview, mrp_workorder, quality_check, mrp_production_summary
 from . import accountmapping, report_mrp_order_detailed, helpdesk_ticket, purchase_open_lines_report, quality_alert
-from . import jamisbill, accountmove, cyclecount
+from . import jamisbill, accountmove, cyclecount, mrp_production_location_change
