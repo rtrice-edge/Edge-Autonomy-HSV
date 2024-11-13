aeea4296027a86628311ba2daf2e17a986842b8c
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2024-11-12 16:16:43 -0800

    attempt #2

--- edge_module/models/mrp_production_location_change.py
+++ edge_module/models/mrp_production_location_change.py
@@ -1,17 +1,30 @@
-from odoo import models, fields
+# models/mrp_location_change.py
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 
-class MrpProductionLocationChange(models.TransientModel):
+class MrpProductionLocationChange(models.Model):
     _name = 'mrp.production.location.change'
-    _description = 'Change Manufacturing Order Locations'
+    _description = 'Manufacturing Order Location Change'
+    _rec_name = 'production_id'
+    _order = 'id desc'
 
     production_id = fields.Many2one('mrp.production', string='Manufacturing Order', required=True)
     location_src_id = fields.Many2one('stock.location', string='Source Location', required=True)
     location_dest_id = fields.Many2one('stock.location', string='Destination Location', required=True)
-
-    def action_change_locations(self):
+    date = fields.Datetime(string='Change Date', default=fields.Datetime.now, readonly=True)
+    user_id = fields.Many2one('res.users', string='Changed By', default=lambda self: self.env.user, readonly=True)
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('done', 'Done')
+    ], string='Status', default='draft', readonly=True)
+    
+    def action_apply_changes(self):
         self.ensure_one()
         production = self.production_id
         
+        if production.state in ('done', 'cancel'):
+            raise UserError('Cannot change locations of completed or cancelled manufacturing orders.')
+            
         # Update MO locations
         production.write({
             'location_src_id': self.location_src_id.id,
@@ -19,7 +32,7 @@ class MrpProductionLocationChange(models.TransientModel):
         })
 
         # Update stock moves if MO is in progress
-        if production.state not in ('draft', 'cancel'):
+        if production.state not in ('draft'):
             production_location = self.env.ref('stock.location_production')
             
             # Update component moves (going to production)
@@ -39,5 +52,6 @@ class MrpProductionLocationChange(models.TransientModel):
                 'location_id': production_location.id,
                 'location_dest_id': self.location_dest_id.id,
             })
-
-        return {'type': 'ir.actions.act_window_close'}
+            
+        self.write({'state': 'done'})
+        return True
