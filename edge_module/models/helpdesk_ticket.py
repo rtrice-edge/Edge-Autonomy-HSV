932a22ee86614759951206314aad49f7055c95a4
Richard Trice <rtrice@edgeautonomy.io>
2024-11-25 11:03:16 -0800

    stupid trick

--- edge_module/models/helpdesk_ticket.py
+++ edge_module/models/helpdesk_ticket.py
@@ -84,7 +84,6 @@ class HelpdeskTicket(models.Model):
             })
             ticket.message_subscribe(partner_ids=[partner.id])
         
-        from teams_integration import send_message_to_teams
         # Replace 'your_webhook_url' with the URL you copied when you created the webhook
         webhook_url = "https://jenaero.webhook.office.com/webhookb2/b420a0ff-bfb1-4d82-8346-e290d675b0d7@1876f61a-3bdb-4843-ae70-75ed0ccb7404/IncomingWebhook/8c62e8c5b8dc40698beeae3e8b4adeed/1f4d666c-833c-435c-aab4-6b002d4a2b44/V2vXokV2krzuxCySmW6FBn3P573ZyzQE6P9JnRpV9P8oU1"
 
