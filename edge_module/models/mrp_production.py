aa28c3d9da61f2077893d75932caf677f37b022e
Richard Trice <rtrice@edgeautonomy.io>
2024-11-25 07:50:33 -0800

    Let's test the scrap all and cancel

--- edge_module/models/mrp_production.py
+++ edge_module/models/mrp_production.py
@@ -44,7 +44,38 @@ class MrpProduction(models.Model):
             raise UserError(_("Only authorized users can modify completed MOs"))
         return super().write(vals)
             
+    
+    def action_scrap_all_and_cancel(self):
+        self.ensure_one()
+        if self.state in ['done', 'cancel']:
+            raise UserError(_("Cannot scrap products for completed or cancelled manufacturing orders."))
+            
+        # Get all storable move_raw_ids
+        moves_to_scrap = self.move_raw_ids.filtered(
+            lambda m: m.product_id.type == 'product' and m.state not in ('done', 'cancel')
+        )
         
+        # Create scrap for each move
+        Scrap = self.env['stock.scrap']
+        for move in moves_to_scrap:
+            scrap_vals = {
+                'production_id': self.id,
+                'product_id': move.product_id.id,
+                'product_uom_id': move.product_uom.id,
+                'scrap_qty': move.product_uom_qty - move.quantity_done,
+                'location_id': move.location_id.id,
+                'lot_id': move.lot_id.id if move.lot_id else False,
+            }
+            scrap = Scrap.create(scrap_vals)
+            scrap.action_validate()
+            
+        # Cancel the MO
+        self.action_cancel()
+        
+        return {'type': 'ir.actions.act_window_close'}
+    
+    
+      
     @api.model
     def create(self, vals):
         # First create the MO using the standard method
