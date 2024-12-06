bdb7d9beb6a1975af6d59af56a870cecad9870ac
Joseph Macfarlane <tennisjoepro@gmail.com>
2024-12-05 17:14:48 -0800

    ok this

--- edge_module/models/mrp_bom_line.py
+++ edge_module/models/mrp_bom_line.py
@@ -5,5 +5,4 @@ class MrpBom(models.Model):
 
     notes = fields.Text(string='Notes')
     product_type = fields.Selection(related='product_id.type', string='Component Type', readonly=True)
-    # a new kit field that will be used to identify if the product is a kit or not
-    kit = fields.Boolean(string='Kit')
+    kit = fields.Boolean(string='Kit', default=False)
