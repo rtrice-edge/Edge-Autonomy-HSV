5159e10c0a84780dbb0aa72ea7bd839f48b95e03
Joseph Macfarlane <tennisjoepro@gmail.com>
2024-12-05 16:57:29 -0800

    added kit field to bom line

--- edge_module/models/mrp_bom_line.py
+++ edge_module/models/mrp_bom_line.py
@@ -5,3 +5,5 @@ class MrpBom(models.Model):
 
     notes = fields.Text(string='Notes')
     product_type = fields.Selection(related='product_id.type', string='Component Type', readonly=True)
+    # a new kit field that will be used to identify if the product is a kit or not
+    kit = fields.Boolean(string='Kit')
