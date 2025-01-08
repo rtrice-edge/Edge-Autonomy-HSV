0a2a56de06aaa56c5596349dacca815ae4a3ad3d
Richard Trice <rtrice@edgeautonomy.io>
2025-01-08 13:29:13 -0800

    test

--- edge_module/models/historical_stock_report.py
+++ edge_module/models/historical_stock_report.py
@@ -16,20 +16,19 @@ class HistoricalStockReport(models.TransientModel):
         stock_move_line_model = self.env['stock.move.line']
         products = {}
 
-        # Search for relevant stock move lines
+        # Search for relevant stock move lines including sub-locations
         stock_move_lines = stock_move_line_model.search(
             [
                 '|',
-                ('location_dest_id', '=', location_id),
-                ('location_id', '=', location_id),
+                ('location_dest_id', 'child_of', location_id),
+                ('location_id', 'child_of', location_id),
                 ('state', '=', 'done'),
                 ('date', '<=', date),
                 ('product_id.type', '=', 'product'),  # Filter for storable products only
             ],
             order="date asc"
         )
-
-        _logger.info("Number of stock move lines found: %d", len(stock_move_lines))
+        _logger.debug("Number of stock move lines found: %d", len(stock_move_lines))
 
         for line in stock_move_lines:
             product_id = line.product_id.id
@@ -40,19 +39,21 @@ class HistoricalStockReport(models.TransientModel):
                     'uom': line.product_uom_id.name,
                     'quantity': 0,
                     'cost': line.product_id.standard_price,  # Adjust for FIFO/AVCO if needed
+                    'location_name': line.location_dest_id.complete_name,  # Full location name
+                    'report_date': date,
                 }
-                _logger.info("Initialized product ID %s: %s", product_id, products[product_id])
+                _logger.debug("Initialized product ID %s: %s", product_id, products[product_id])
 
             if line.location_dest_id.id == location_id:
                 products[product_id]['quantity'] += line.qty_done
-                _logger.info("Added quantity to product %s: %s", line.product_id.default_code, line.qty_done)
+                _logger.debug("Added quantity to product %s: %s", product_id, line.qty_done)
             if line.location_id.id == location_id:
                 products[product_id]['quantity'] -= line.qty_done
-                _logger.info("Subtracted quantity from product %s: %s", line.product_id.default_code, line.qty_done)
+                _logger.debug("Subtracted quantity from product %s: %s", product_id, line.qty_done)
 
         for product_id, product in products.items():
             product['total_value'] = product['quantity'] * product['cost']
-            _logger.info("Calculated total value for product %s: %s", product['default_code'], product['total_value'])
+            _logger.debug("Calculated total value for product %s: %s", product_id, product['total_value'])
 
         _logger.info("Completed stock fetching. Total products processed: %d", len(products))
         return list(products.values())
@@ -76,6 +77,8 @@ class HistoricalStockReport(models.TransientModel):
                 'quantity': data['quantity'],
                 'cost': data['cost'],
                 'total_value': data['total_value'],
+                'location_name': data['location_name'],
+                'report_date': data['report_date'],
             })
             _logger.info("Created report line for product: %s", data['default_code'])
 
@@ -99,3 +102,6 @@ class HistoricalStockReportLine(models.Model):
     quantity = fields.Float(string='Quantity')
     cost = fields.Float(string='Cost')
     total_value = fields.Float(string='Total Value')
+    location_name = fields.Char(string='Location')  # Actual location name
+    report_date = fields.Date(string='Report Date')  # Date selected in the wizard
+
