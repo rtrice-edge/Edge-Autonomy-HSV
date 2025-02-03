79df1bf75ffaea838d6b2d2072ee8c17f3a52cd9
Richard Trice <rtrice@edgeautonomy.io>
2025-02-03 10:22:38 -0800

    asdfasdf

--- edge_module/models/mrp_bom.py
+++ edge_module/models/mrp_bom.py
@@ -8,74 +8,62 @@ class MrpBom(models.Model):
 
     is_rma_bom = fields.Boolean(
         string="RMA BOM",
-        help="If checked, the produced product will be automatically added as a component in this BOM."
+        help="If checked, the BOM Product is automatically added as a BOM line."
     )
-
-    @api.onchange('is_rma_bom')
-    def _onchange_is_rma_bom(self):
-        _logger.info("Onchange triggered for BOM (id: %s): is_rma_bom set to %s", self.id, self.is_rma_bom)
 
     @api.model
     def create(self, vals):
-        _logger.info("Creating BOM with vals: %s", vals)
         bom = super(MrpBom, self).create(vals)
         _logger.info("Created BOM (id: %s) with is_rma_bom=%s", bom.id, bom.is_rma_bom)
         if bom.is_rma_bom and bom.product_id:
-            _logger.info("BOM (id: %s) is marked as RMA BOM and has a product (%s). Adding auto RMA BOM line.", bom.id, bom.product_id.display_name)
-            bom._update_rma_bom_line(add=True)
+            _logger.info("BOM (id: %s): RMA BOM enabled; adding BOM product (%s) as auto line.",
+                         bom.id, bom.product_id.display_name)
+            bom._update_bom_product_line(add=True)
         return bom
 
     def write(self, vals):
-        _logger.info("Write called on BOM (id(s): %s) with vals: %s", self.ids, vals)
         res = super(MrpBom, self).write(vals)
         for bom in self:
-            # If key fields change, update auto RMA BOM line accordingly.
-            if ('is_rma_bom' in vals) or any(key in vals for key in ['product_id', 'product_qty', 'product_uom_id']):
-                _logger.info("Updating BOM (id: %s) after write with vals: %s", bom.id, vals)
+            _logger.info("BOM (id: %s) write() called with vals: %s", bom.id, vals)
+            # If any of these fields are updated, adjust the auto BOM line
+            if any(field in vals for field in ['is_rma_bom', 'product_id', 'product_qty', 'product_uom_id']):
                 if bom.is_rma_bom and bom.product_id:
-                    # Remove any auto-lines that refer to a different product.
-                    auto_wrong = bom.bom_line_ids.filtered(lambda l: l.auto_rma and l.product_id != bom.product_id)
-                    if auto_wrong:
-                        _logger.info("BOM (id: %s) has %s auto RMA BOM line(s) for an outdated product. Removing them.", bom.id, len(auto_wrong))
-                        auto_wrong.unlink()
-                    # Update or create the auto RMA BOM line.
-                    auto_lines = bom.bom_line_ids.filtered(lambda l: l.auto_rma and l.product_id == bom.product_id)
-                    if auto_lines:
-                        _logger.info("Updating existing auto RMA BOM line(s) for BOM (id: %s).", bom.id)
-                        auto_lines.write({
-                            'product_qty': bom.product_qty,
-                            'product_uom_id': bom.product_uom_id.id,
-                        })
-                    else:
-                        _logger.info("No auto RMA BOM line found for BOM (id: %s). Creating one.", bom.id)
-                        bom._update_rma_bom_line(add=True)
+                    _logger.info("BOM (id: %s): RMA BOM enabled; ensuring BOM product auto line exists.", bom.id)
+                    bom._update_bom_product_line(add=True)
                 else:
-                    _logger.info("BOM (id: %s) is not marked as RMA BOM or does not have a product. Removing any auto RMA BOM lines if they exist.", bom.id)
-                    bom._update_rma_bom_line(add=False)
+                    _logger.info("BOM (id: %s): RMA BOM disabled; removing BOM product auto line.", bom.id)
+                    bom._update_bom_product_line(add=False)
         return res
 
-    def _update_rma_bom_line(self, add=True):
-        """Helper method to add or remove the auto-created BOM line for the produced product."""
-        auto_lines = self.bom_line_ids.filtered(lambda l: l.auto_rma and l.product_id == self.product_id)
+    def _update_bom_product_line(self, add=True):
+        """
+        Helper method to add or remove the auto-generated BOM line for the BOM product.
+        When add=True, if an auto line for the BOM product does not exist, it is created.
+        When add=False, the auto line (if any) is removed.
+        """
+        auto_line = self.bom_line_ids.filtered(lambda l: l.auto_rma and l.product_id == self.product_id)
         if add:
-            if not auto_lines and self.product_uom_id and self.product_id:
-                _logger.info("Creating auto RMA BOM line for BOM (id: %s).", self.id)
+            if auto_line:
+                _logger.info("BOM (id: %s): Auto BOM line exists; updating quantity and UoM.", self.id)
+                auto_line.write({
+                    'product_qty': self.product_qty,
+                    'product_uom_id': self.product_uom_id.id,
+                })
+            else:
+                _logger.info("BOM (id: %s): Creating auto BOM line for BOM product (id: %s).",
+                             self.id, self.product_id.id)
                 self.env['mrp.bom.line'].create({
                     'bom_id': self.id,
                     'product_id': self.product_id.id,
                     'product_qty': self.product_qty,
                     'product_uom_id': self.product_uom_id.id,
                     'auto_rma': True,
-                    'name': _('RMA Work - Self Consumption'),
+                    'name': _('RMA Work - BOM Product'),
                 })
-            else:
-                _logger.info("Auto RMA BOM line already exists for BOM (id: %s), skipping creation.", self.id)
         else:
-            if auto_lines:
-                _logger.info("Removing %s auto RMA BOM line(s) from BOM (id: %s).", len(auto_lines), self.id)
-                auto_lines.unlink()
+            if auto_line:
+                _logger.info("BOM (id: %s): Removing auto BOM line.", self.id)
+                auto_line.unlink()
             else:
-                _logger.info("No auto RMA BOM lines to remove for BOM (id: %s).", self.id)
-
-
+                _logger.info("BOM (id: %s): No auto BOM line found to remove.", self.id)
 
