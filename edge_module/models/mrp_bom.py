3fc96dde3c615e46bd0151b71f120bf6d91141c8
Richard Trice <rtrice@edgeautonomy.io>
2025-02-03 10:53:52 -0800

    test

--- edge_module/models/mrp_bom.py
+++ edge_module/models/mrp_bom.py
@@ -11,50 +11,66 @@ class MrpBom(models.Model):
         help="If checked, the BOM Product is automatically added as a BOM line."
     )
 
+    def _get_produced_product(self):
+        """Return the produced product:
+           - Use product_id if set,
+           - Otherwise, if product_tmpl_id is set, return its product variant.
+        """
+        self.ensure_one()
+        produced_product = self.product_id or (self.product_tmpl_id and self.product_tmpl_id.product_variant_id)
+        return produced_product
+
     @api.model
     def create(self, vals):
+        _logger.info("Creating BOM with vals: %s", vals)
         bom = super(MrpBom, self).create(vals)
-        _logger.info("Created BOM (id: %s) with is_rma_bom=%s", bom.id, bom.is_rma_bom)
-        if bom.is_rma_bom and bom.product_id:
-            _logger.info("BOM (id: %s): RMA BOM enabled; adding BOM product (%s) as auto line.",
-                         bom.id, bom.product_id.display_name)
+        produced_product = bom._get_produced_product()
+        _logger.info("Created BOM (id: %s) with is_rma_bom=%s, produced_product=%s", 
+                     bom.id, bom.is_rma_bom, produced_product.id if produced_product else 'None')
+        if bom.is_rma_bom and produced_product:
+            _logger.info("BOM (id: %s): RMA BOM enabled; adding produced product (%s) as auto line.",
+                         bom.id, produced_product.display_name)
             bom._update_bom_product_line(add=True)
         return bom
 
     def write(self, vals):
         res = super(MrpBom, self).write(vals)
         for bom in self:
-            _logger.info("BOM (id: %s) write() called with vals: %s", bom.id, vals)
+            produced_product = bom._get_produced_product()
+            _logger.info("After write, BOM (id: %s): is_rma_bom=%s, produced_product=%s", 
+                         bom.id, bom.is_rma_bom, produced_product.id if produced_product else 'None')
             # If any of these fields are updated, adjust the auto BOM line
-            if any(field in vals for field in ['is_rma_bom', 'product_id', 'product_qty', 'product_uom_id']):
-                if bom.is_rma_bom and bom.product_id:
-                    _logger.info("BOM (id: %s): RMA BOM enabled; ensuring BOM product auto line exists.", bom.id)
+            if any(field in vals for field in ['is_rma_bom', 'product_id', 'product_tmpl_id', 'product_qty', 'product_uom_id']):
+                if bom.is_rma_bom and produced_product:
+                    _logger.info("BOM (id: %s): RMA BOM enabled; ensuring produced product auto line exists.", bom.id)
                     bom._update_bom_product_line(add=True)
                 else:
-                    _logger.info("BOM (id: %s): RMA BOM disabled; removing BOM product auto line.", bom.id)
+                    _logger.info("BOM (id: %s): RMA BOM disabled; removing produced product auto line.", bom.id)
                     bom._update_bom_product_line(add=False)
         return res
 
     def _update_bom_product_line(self, add=True):
         """
-        Helper method to add or remove the auto-generated BOM line for the BOM product.
-        When add=True, if an auto line for the BOM product does not exist, it is created.
+        Helper method to add or remove the auto-generated BOM line for the produced product.
+        When add=True, if an auto line for the produced product does not exist, it is created.
         When add=False, the auto line (if any) is removed.
         """
-        auto_line = self.bom_line_ids.filtered(lambda l: l.auto_rma and l.product_id == self.product_id)
+        produced_product = self._get_produced_product()
+        auto_line = self.bom_line_ids.filtered(lambda l: l.auto_rma and l.product_id == produced_product)
         if add:
             if auto_line:
-                _logger.info("BOM (id: %s): Auto BOM line exists; updating quantity and UoM.", self.id)
+                _logger.info("BOM (id: %s): Auto BOM line exists; updating quantity (%s) and UoM (%s).",
+                             self.id, self.product_qty, self.product_uom_id.id)
                 auto_line.write({
                     'product_qty': self.product_qty,
                     'product_uom_id': self.product_uom_id.id,
                 })
             else:
-                _logger.info("BOM (id: %s): Creating auto BOM line for BOM product (id: %s).",
-                             self.id, self.product_id.id)
+                _logger.info("BOM (id: %s): Creating auto BOM line for produced product (id: %s).",
+                             self.id, produced_product.id)
                 self.env['mrp.bom.line'].create({
                     'bom_id': self.id,
-                    'product_id': self.product_id.id,
+                    'product_id': produced_product.id,
                     'product_qty': self.product_qty,
                     'product_uom_id': self.product_uom_id.id,
                     'auto_rma': True,
