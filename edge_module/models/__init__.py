1f127dd8f5fce25278421e2a3e4f7a4dcb0a06ca
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-02-14 14:11:20 -0800

    fixing init

--- edge_module/models/__init__.py
+++ edge_module/models/__init__.py
@@ -5,5 +5,5 @@ from . import sale_order_line, stock_move_line, stock_move, stock_picking, stock
 from . import trade_show_shipment, purchase_requisition, mrp_production, demand, componentmoview, mrp_workorder, quality_check, mrp_production_summary
 from . import accountmapping, report_mrp_order_detailed, helpdesk_ticket, purchase_open_lines_report, quality_alert
 from . import jamisbill, accountmove, cyclecount, mrp_production_location_change, mrp_additional_consumption_wizard , report_mrp_order_components
-from . import historical_stock_report, kit_dashboard, mrp_bom, cycle_count_accuracy_view, accountmoveline, purchase_request, purchase_requeset_line
+from . import historical_stock_report, kit_dashboard, mrp_bom, cycle_count_accuracy_view, accountmoveline, purchase_request, purchase_request_line
 from . import approval_matrix, purchase_request_approver
