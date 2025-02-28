1ce7c21dc712961d2aa3f098b860a6fffe66dd3c
Richard Trice <rtrice@edgeautonomy.io>
2025-02-28 11:29:13 -0800

    Alright... now

--- edge_module/__manifest__.py
+++ edge_module/__manifest__.py
@@ -13,6 +13,7 @@
             'edge_module/static/src/js/custom_form_view.js',
             'edge_module/static/src/css/custom_styles.css',
             'edge_module/static/src/images/HSV_background1.jpg',
+            'edge_module/static/src/js/action_manager_report.esm.js',
         ],
     },
     'data': [
