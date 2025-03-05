b80c6cb489eb6ccd0b4d8be083cb360562ac4552
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-03-05 14:17:07 -0800

    no, no, no I wasn't wrong ... I was just testing you

--- edge_module/models/historical_po_wizard.py
+++ edge_module/models/historical_po_wizard.py
@@ -14,11 +14,11 @@ class HistoricalPurchaseLinesWizard(models.TransientModel):
     def action_view_historical_lines(self):
         """Open purchase order lines view with historical date context"""
         self.ensure_one()
-        action = self.env.ref('purchase.purchase_order_line_action').read()[0]
+        action = self.env.ref('edge_module.action_historical_purchase_order_lines').read()[0]
         action['context'] = dict(self.env.context)
         action['context'].update({
             'historical_date': self.date,
-            'search_default_open_pos': 1  # Apply filter for open orders
+            'search_default_historical_not_received': 1  # Apply filter for open orders
         })
         action['name'] = f'Open Purchase Lines as of {self.date}'
         return action
