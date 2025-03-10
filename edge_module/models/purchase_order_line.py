200dec8a5dc6ee9f02c9b43c23d1623499e4ab86
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-03-10 12:47:36 -0700

    adding hist values

--- edge_module/models/purchase_order_line.py
+++ edge_module/models/purchase_order_line.py
@@ -504,6 +504,23 @@ class PurchaseOrderLine(models.Model):
 
     
     
+    @api.depends('product_qty', 'price_unit', 'qty_received', 'move_ids', 'move_ids.state', 'move_ids.date', 'order_id.state')
+    def _compute_historical_values(self):
+        # This method will be called automatically by Odoo for computing the stored fields
+        # Get the historical date from context or use current date
+        historical_date = self.env.context.get('historical_date')
+        
+        if not historical_date:
+            # If no historical date in context, just use current values
+            for line in self:
+                line.historical_qty_open = line.qty_open
+                line.historical_open_cost = line.open_cost
+                line.historical_receipt_status = line.line_receipt_status
+            return
+    
+        # If historical date exists, call the forced computation
+        self.compute_historical_values_forced()
+
     # New fields specifically for historical view
     historical_qty_open = fields.Float(string='Historical Open Qty', compute='_compute_historical_values', store=True)
     historical_open_cost = fields.Float(string='Historical Open Cost', compute='_compute_historical_values', store=True)
@@ -513,6 +530,8 @@ class PurchaseOrderLine(models.Model):
         ('full', 'Fully Received'),
         ('cancel', 'Cancelled')
     ], string='Historical Status', compute='_compute_historical_values', store=True)
+
+
 
 
     def compute_historical_values_forced(self):
