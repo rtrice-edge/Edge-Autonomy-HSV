894b8e4236dd4f385343914884d72917ef54b9f4
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-03-24 16:41:26 -0700

    ensuring force close po button actually marks po as fully received

--- edge_module/models/purchase_order.py
+++ edge_module/models/purchase_order.py
@@ -380,6 +380,10 @@ class AdministrativeClosureWizard(models.TransientModel):
         for picking in stock_pickings:
             picking.action_cancel()
 
+        # if order is still not recognizing that it has been fully received, mark it as fully received
+        if (purchase_order.receipt_status == 'pending' or purchase_order.receipt_status == 'partial'):
+            purchase_order.receipt_status = 'full'
+
         # Set the flag that our compute method checks
         purchase_order.write({'admin_closed': True})
 
