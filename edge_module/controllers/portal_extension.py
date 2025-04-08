ccff361e3312de61a02fe416ee88ab9da3a32159
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-04-08 14:57:37 -0700

    test

--- edge_module/controllers/portal_extension.py
+++ edge_module/controllers/portal_extension.py
@@ -13,17 +13,22 @@ class HomeExtended(Home):
         _logger.info("------------- index method called --------------------")
         _logger.info("PORTAL EXTENSTION CONTROLLER: User ID: %s", request.session.uid)
         if request.session.uid:
-            user = request.env['res.users'].browse(request.session.uid)
+            # Use sudo() to avoid permission issues
+            user = request.env['res.users'].sudo().browse(request.session.uid)
             _logger.info("User ID: %s", user.id)
             _logger.info("User Name: %s", user.name)
             
-            # Skip the override for admin and internal users - IMPORTANT!
+            # Skip the override for admin and internal users
             if user.has_group('base.group_system') or user.has_group('base.group_user'):
                 _logger.info("User is admin or internal - using standard behavior")
                 return super().index(*args, **kw)
                 
             _logger.info("User Groups: %s", user.groups_id.mapped('name'))
-            if user.has_group('base.group_portal') and self._is_purchase_approver(user):
+            # Check if portal user and approver using sudo
+            is_portal = user.has_group('base.group_portal')
+            is_approver = self._is_purchase_approver(user)
+            
+            if is_portal and is_approver:
                 _logger.info("User is a portal user and purchase approver - redirecting to purchase requests")
                 return http.request.redirect('/web#cids=1&action=edge_module.action_purchase_request')
         return super().index(*args, **kw)
@@ -33,17 +38,22 @@ class HomeExtended(Home):
         _logger.info("-------------- web client method called -------------------------")
         _logger.info("PORTAL EXTENSTION CONTROLLER: User ID: %s", request.session.uid)
         if request.session.uid:
-            user = request.env['res.users'].browse(request.session.uid)
+            # Use sudo() to avoid permission issues
+            user = request.env['res.users'].sudo().browse(request.session.uid)
             _logger.info("User ID: %s", user.id)
             _logger.info("User Name: %s", user.name)
             
-            # Skip the override for admin and internal users - IMPORTANT!
+            # Skip the override for admin and internal users
             if user.has_group('base.group_system') or user.has_group('base.group_user'):
                 _logger.info("User is admin or internal - using standard behavior")
                 return super().web_client(s_action=s_action, **kw)
                 
             _logger.info("User Groups: %s", user.groups_id.mapped('name'))
-            if user.has_group('base.group_portal') and self._is_purchase_approver(user):
+            # Check if portal user and approver using sudo
+            is_portal = user.has_group('base.group_portal')
+            is_approver = self._is_purchase_approver(user)
+            
+            if is_portal and is_approver:
                 _logger.info("User is a portal user and purchase approver")
                 if not s_action:
                     _logger.info("s_action is None, setting it to edge_module.action_purchase_request")
@@ -54,6 +64,7 @@ class HomeExtended(Home):
     def _is_purchase_approver(self, user):
         _logger.info("Checking if user is a purchase approver")
         """Check if user is listed as an approver in any purchase request"""
+        # Use sudo() to avoid permission issues
         approver_records = request.env['purchase.request.approver'].sudo().search([
             ('user_id', '=', user.id)
         ])
