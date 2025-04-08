cf8388673c74db950d5c8e4576e258bc2d2dda3a
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-04-08 14:18:10 -0700

    test

--- edge_module/controllers/web_controller_extension.py
+++ edge_module/controllers/web_controller_extension.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+from odoo import http, _
 from odoo.http import request
 from odoo.addons.web.controllers.home import Home
 
-class HomeExtension(Home):
+class HomeExtended(Home):
     @http.route()
     def index(self, *args, **kw):
-        if request.session.uid and self._is_purchase_approver(request.session.uid):
-            return http.request.redirect('/web#action=edge_module.action_purchase_request')
-        return super().index(*args, **kw)
-
+        if request.session.uid and not self._is_public_user():
+            user = request.env['res.users'].browse(request.session.uid)
+            if self._is_purchase_approver(user):
+                return http.request.redirect('/web#cids=1&menu_id=208&action=edge_module.action_purchase_request')
+        return super(HomeExtended, self).index(*args, **kw)
+    
     @http.route()
     def web_client(self, s_action=None, **kw):
-        if request.session.uid and self._is_purchase_approver(request.session.uid):
-            return super().web_client(s_action, **kw)
-        return super().web_client(s_action, **kw)
-
-    def _is_purchase_approver(self, uid):
-        # Check if the user is in the portal purchase approver group
-        user = request.env['res.users'].sudo().browse(uid)
-        return user.has_group('edge_module.group_portal_purchase_approver')
+        if request.session.uid and not self._is_public_user():
+            user = request.env['res.users'].browse(request.session.uid)
+            if self._is_purchase_approver(user) and not s_action:
+                s_action = 'edge_module.action_purchase_request'
+        return super(HomeExtended, self).web_client(s_action=s_action, **kw)
+    
+    def _is_public_user(self):
+        return request.env.user._is_public()
+    
+    def _is_purchase_approver(self, user):
+        """Check if user is listed as an approver in any purchase request"""
+        if user.has_group('base.group_portal'):
+            approver_records = request.env['purchase.request.approver'].sudo().search([
+                ('user_id', '=', user.id)
+            ])
+            return bool(approver_records)
+        return False
