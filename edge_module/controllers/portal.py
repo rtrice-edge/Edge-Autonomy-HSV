e115d3a44a50df8e1e7b106d176afc736d6f815f
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-04-09 13:29:03 -0700

    test

--- edge_module/controllers/portal.py
+++ edge_module/controllers/portal.py
@@ -14,17 +14,28 @@ class PurchaseRequestPortal(CustomerPortal):
             partner = request.env.user.partner_id
             PurchaseRequest = request.env['purchase.request']
             
-            domain = [
-                '|', '|', '|', '|', '|',
-                ('approver_level_1.user_id.partner_id', '=', partner.id),
-                ('approver_level_2.user_id.partner_id', '=', partner.id),
-                ('approver_level_3.user_id.partner_id', '=', partner.id),
-                ('approver_level_4.user_id.partner_id', '=', partner.id),
-                ('approver_level_5.user_id.partner_id', '=', partner.id),
+            # Similar logic as the list view
+            request_ids = []
+            pending_requests = PurchaseRequest.sudo().search([
                 ('state', '=', 'pending_approval')
-            ]
+            ])
             
-            values['purchase_request_count'] = PurchaseRequest.search_count(domain)
+            for pr in pending_requests:
+                for i in range(1, 13):
+                    needs_approver_field = f'needs_approver_level_{i}'
+                    is_approved_field = f'is_level_{i}_approved'
+                    approver_field = f'approver_level_{i}'
+                    
+                    if hasattr(pr, needs_approver_field) and getattr(pr, needs_approver_field):
+                        if hasattr(pr, is_approved_field) and not getattr(pr, is_approved_field):
+                            if hasattr(pr, approver_field):
+                                approver = getattr(pr, approver_field)
+                                if approver and approver.user_id.partner_id.id == partner.id:
+                                    request_ids.append(pr.id)
+                                    break
+                            break
+            
+            values['purchase_request_count'] = len(request_ids)
         
         return values
 
@@ -34,15 +45,39 @@ class PurchaseRequestPortal(CustomerPortal):
         partner = request.env.user.partner_id
         PurchaseRequest = request.env['purchase.request']
         
-        domain = [
-            '|', '|', '|', '|', '|',
-            ('approver_level_1.user_id.partner_id', '=', partner.id),
-            ('approver_level_2.user_id.partner_id', '=', partner.id),
-            ('approver_level_3.user_id.partner_id', '=', partner.id),
-            ('approver_level_4.user_id.partner_id', '=', partner.id),
-            ('approver_level_5.user_id.partner_id', '=', partner.id),
+        # We'll build a list of IDs of purchase requests requiring this user's approval
+        request_ids = []
+        
+        # First get all purchase requests that are in pending_approval state
+        pending_requests = PurchaseRequest.sudo().search([
             ('state', '=', 'pending_approval')
-        ]
+        ])
+        
+        # Now filter for only those where this user is the next required approver
+        for pr in pending_requests:
+            for i in range(1, 13):  # Loop through all possible approver levels
+                needs_approver_field = f'needs_approver_level_{i}'
+                is_approved_field = f'is_level_{i}_approved'
+                approver_field = f'approver_level_{i}'
+                
+                # Check if we need this level of approval
+                if hasattr(pr, needs_approver_field) and getattr(pr, needs_approver_field):
+                    # Check if this level is not yet approved
+                    if hasattr(pr, is_approved_field) and not getattr(pr, is_approved_field):
+                        # Check if the current user is the approver at this level
+                        if hasattr(pr, approver_field):
+                            approver = getattr(pr, approver_field)
+                            if approver and approver.user_id.partner_id.id == partner.id:
+                                # This PR needs this user's approval next
+                                request_ids.append(pr.id)
+                                break  # Break after finding the first level that needs approval
+                        
+                        # If we've found a level that needs approval but the current 
+                        # user is not the approver, break the loop - no need to check further
+                        break
+        
+        # Now build the domain using the filtered IDs
+        domain = [('id', 'in', request_ids)]
         
         # Sorting options
         searchbar_sortings = {
@@ -57,7 +92,7 @@ class PurchaseRequestPortal(CustomerPortal):
         order = searchbar_sortings[sortby]['order']
         
         # Count for pager
-        purchase_request_count = PurchaseRequest.search_count(domain)
+        purchase_request_count = len(request_ids)
         
         # Pager
         pager = portal_pager(
@@ -94,25 +129,36 @@ class PurchaseRequestPortal(CustomerPortal):
         except (AccessError, MissingError):
             return request.redirect('/my')
             
-        # Check if the user is an approver for this purchase request
+        # Check if the user is the next required approver for this purchase request
         partner = request.env.user.partner_id
-        is_approver = False
+        is_next_approver = False
         
-        for i in range(1, 6):
+        # Only allow viewing if the request is in pending_approval state
+        if purchase_request_sudo.state != 'pending_approval':
+            return request.redirect('/my')
+        
+        # Check if user is the next required approver
+        for i in range(1, 13):
+            needs_approver_field = f'needs_approver_level_{i}'
+            is_approved_field = f'is_level_{i}_approved'
             approver_field = f'approver_level_{i}'
-            if hasattr(purchase_request_sudo, approver_field):
-                approver = getattr(purchase_request_sudo, approver_field)
-                if approver and approver.user_id.partner_id.id == partner.id:
-                    is_approver = True
+            
+            if hasattr(purchase_request_sudo, needs_approver_field) and getattr(purchase_request_sudo, needs_approver_field):
+                if hasattr(purchase_request_sudo, is_approved_field) and not getattr(purchase_request_sudo, is_approved_field):
+                    if hasattr(purchase_request_sudo, approver_field):
+                        approver = getattr(purchase_request_sudo, approver_field)
+                        if approver and approver.user_id.partner_id.id == partner.id:
+                            is_next_approver = True
+                            break
                     break
         
-        if not is_approver and purchase_request_sudo.state != 'pending_approval':
+        if not is_next_approver:
             return request.redirect('/my')
             
         values = {
             'purchase_request': purchase_request_sudo,
             'page_name': 'purchase_request_detail',
-            'is_approver': is_approver,
+            'is_approver': is_next_approver,
         }
         
         return request.render("edge_module.portal_my_purchase_request_detail", values)
@@ -124,32 +170,47 @@ class PurchaseRequestPortal(CustomerPortal):
         except (AccessError, MissingError):
             return request.redirect('/my')
             
-        # Check if the user is an approver for this purchase request
+        # Check if the user is the next required approver for this purchase request
         partner = request.env.user.partner_id
         can_approve = False
+        approver_level = None
         
-        # Find which approver level the current user is
-        for i in range(1, 6):
-            approver_field = f'approver_level_{i}'
+        # Only allow approval if the request is in pending_approval state
+        if purchase_request_sudo.state != 'pending_approval':
+            return request.redirect('/my')
+        
+        # Check if user is the next required approver
+        for i in range(1, 13):
+            needs_approver_field = f'needs_approver_level_{i}'
             is_approved_field = f'is_level_{i}_approved'
+            approver_field = f'approver_level_{i}'
             
-            if hasattr(purchase_request_sudo, approver_field) and hasattr(purchase_request_sudo, is_approved_field):
-                approver = getattr(purchase_request_sudo, approver_field)
-                is_approved = getattr(purchase_request_sudo, is_approved_field)
-                
-                if approver and approver.user_id.partner_id.id == partner.id and not is_approved:
-                    can_approve = True
+            if hasattr(purchase_request_sudo, needs_approver_field) and getattr(purchase_request_sudo, needs_approver_field):
+                if hasattr(purchase_request_sudo, is_approved_field) and not getattr(purchase_request_sudo, is_approved_field):
+                    if hasattr(purchase_request_sudo, approver_field):
+                        approver = getattr(purchase_request_sudo, approver_field)
+                        if approver and approver.user_id.partner_id.id == partner.id:
+                            can_approve = True
+                            approver_level = i
+                            break
                     break
         
-        if can_approve and purchase_request_sudo.state == 'pending_approval':
-            # Call the approve function
-            purchase_request_sudo.sudo().action_approve()
+        if can_approve:
+            # Mark this level as approved
+            setattr(purchase_request_sudo.sudo(), f'is_level_{approver_level}_approved', True)
             
+            # Check if fully approved
+            if purchase_request_sudo.sudo().is_fully_approved():
+                purchase_request_sudo.sudo().write({'state': 'approved'})
+                
             # Post message in chatter
             purchase_request_sudo.sudo().message_post(
-                body=_("Request approved by %s through the portal.") % partner.name,
+                body=_("Level %s approval completed by %s through the portal.") % (approver_level, partner.name),
                 message_type='notification',
                 subtype_xmlid='mail.mt_comment'
             )
             
-        return request.redirect('/my/purchase_requests/%s' % purchase_request_id)
+            # Notify the next approver if there is one
+            purchase_request_sudo.sudo()._notify_next_approver()
+            
+        return request.redirect('/my/purchase_requests')
