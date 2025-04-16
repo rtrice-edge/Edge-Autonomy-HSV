ff2e57edd5135b86fabf27987c52f712d1a49d95
Richard Trice <rtrice@edgeautonomy.io>
2025-04-16 08:36:26 -0700

    trying again.

--- edge_module/models/__init__.py
+++ edge_module/models/__init__.py
@@ -8,3 +8,5 @@ from . import jamisbill, accountmove, cyclecount, mrp_production_location_change
 from . import historical_stock_report, kit_dashboard, mrp_bom, cycle_count_accuracy_view, accountmoveline, purchase_request, purchase_request_line, purchase_request_cancel_wizard
 from . import approval_matrix, purchase_request_approver, quality_point, cyclecountxlsxreport, historical_po_wizard, stock_move_chain_wizard
 from . import stock_quant_history,historical_stock_report
+
+# weird.
