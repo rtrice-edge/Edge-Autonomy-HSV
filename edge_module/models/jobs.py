ff6fa26676dbff73f312c26a0347b085d8da3283
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-04-16 12:29:16 -0700

    adding custom order for jobs

--- edge_module/models/jobs.py
+++ edge_module/models/jobs.py
@@ -3,10 +3,11 @@ from odoo import models, fields, api
 class Jobs(models.Model):
     _name = 'job'
     _description = 'Jobs Management'
-    _order = 'name'
+    _order = 'order'
     
     name = fields.Char(string='Job Name', required=True)
     number = fields.Char(string='Job Number', required=True)
+    order = fields.Integer(string='Order', required=True, default=999)
     active = fields.Boolean(default=True)
     
     _sql_constraints = [
