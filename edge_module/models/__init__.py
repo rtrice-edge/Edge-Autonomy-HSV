3ed1993253831dfb1df0977bd747faf30e089ae6
Richard Trice <rtrice@edgeautonomy.io>
2025-04-16 08:38:02 -0700

    Oooookk?

--- edge_module/models/__init__.py
+++ edge_module/models/__init__.py
@@ -5,8 +5,8 @@ from . import sale_order_line, stock_move_line, stock_move, stock_picking, stock
 from . import trade_show_shipment, purchase_requisition, mrp_production, demand, componentmoview, mrp_workorder, quality_check, mrp_production_summary
 from . import accountmapping, report_mrp_order_detailed, helpdesk_ticket, purchase_open_lines_report, quality_alert
 from . import jamisbill, accountmove, cyclecount, mrp_production_location_change, mrp_additional_consumption_wizard , report_mrp_order_components
-from . import historical_stock_report, kit_dashboard, mrp_bom, cycle_count_accuracy_view, accountmoveline, purchase_request, purchase_request_line, purchase_request_cancel_wizard
+from . import kit_dashboard, mrp_bom, cycle_count_accuracy_view, accountmoveline, purchase_request, purchase_request_line, purchase_request_cancel_wizard
 from . import approval_matrix, purchase_request_approver, quality_point, cyclecountxlsxreport, historical_po_wizard, stock_move_chain_wizard
 from . import stock_quant_history,historical_stock_report
 
-# weird.
+# roving a double include.  
