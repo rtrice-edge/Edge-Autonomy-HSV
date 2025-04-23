4eda0203e06fdbbc0abb5a5ea281f23611cfca05
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-04-23 13:14:04 -0700

    imports

--- edge_module/__manifest__.py
+++ edge_module/__manifest__.py
@@ -120,6 +120,7 @@
         'views/purchase_request/portal_templates.xml',
         'views/purchase_request/cancel_wizard_view.xml',
         'views/purchase_request/purchase_request_efficiency_views.xml',
+        'views/purchase_request/purchase_request_import_wizard.xml',
 
         'views/historical_po_lines/historical_po_wizard_view.xml',
         'views/historical_po_lines/purchase_order_line_search_extend.xml',
