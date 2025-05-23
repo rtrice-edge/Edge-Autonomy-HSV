791e37278e5c553c6c964838f96f6a0070d8389d
Joseph Macfarlane <josephmmacfarlane@gmail.com>
2025-05-23 15:41:34 -0700

    lets try this

--- edge_module/models/__init__.py
+++ edge_module/models/__init__.py
@@ -2,7 +2,7 @@
 
 from . import jobs, purchase_request_efficiency_view, quality_point, mrp_bom_line, product_template, project_project, project_table, purchase_order, purchase_order_line, res_partner
 from . import sale_order_line, stock_move_line, stock_move, stock_picking, stock_quant
-from . import trade_show_shipment, purchase_requisition, mrp_production, demand, demand_sortable, componentmoview, mrp_workorder, quality_check, mrp_production_summary
+from . import trade_show_shipment, purchase_requisition, mrp_production, demand_sortable, componentmoview, mrp_workorder, quality_check, mrp_production_summary
 from . import accountmapping, report_mrp_order_detailed, helpdesk_ticket, purchase_open_lines_report, quality_alert
 from . import jamisbill, accountmove, cyclecount, mrp_production_location_change, mrp_additional_consumption_wizard , report_mrp_order_components
 from . import kit_dashboard, mrp_bom, cycle_count_accuracy_view, accountmoveline, purchase_request, purchase_request_line, purchase_request_cancel_wizard, purchase_request_efficiency_view, purchase_request_efficiency_report, purchase_request_import_wizard
